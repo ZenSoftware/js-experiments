05db3bdbe55f298c018f9aef971b834c3de1c77b
Zen Software <ZenSoftware@users.noreply.github.com>
2022-01-19 21:19:56 -0700

    test description update

--- leet/twoSum.spec.ts
+++ leet/twoSum.spec.ts
@@ -1,6 +1,6 @@
 import { twoSumHash } from './twoSum';
 
-test('should find indexes of sum', () => {
+test('should pass leetcode.com test cases', () => {
   expect(twoSumHash([2, 7, 11, 15], 9)).toEqual([0, 1]);
   expect(twoSumHash([3, 2, 4], 6)).toEqual([1, 2]);
   expect(twoSumHash([3, 3], 6)).toEqual([0, 1]);
