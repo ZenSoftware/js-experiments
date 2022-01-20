ad6cf7dba01cdd0a710a028280a643b00360828d
Zen Software <ZenSoftware@users.noreply.github.com>
2022-01-19 21:12:19 -0700

    added link

--- leet/twoSum.ts
+++ leet/twoSum.ts
@@ -1,3 +1,5 @@
+// https://leetcode.com/problems/two-sum/
+
 export function twoSumBrute(nums: number[], target: number): number[] | void {
   for (let i = 0; i < nums.length - 1; i++) {
     for (let j = i + 1; j < nums.length; j++) {
