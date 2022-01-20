eda4b0a828b686392b4e873e6f7af8d2650a276d
Zen Software <ZenSoftware@users.noreply.github.com>
2022-01-19 17:42:45 -0700

    twoSum

---
+++ src/www/twoSum.ts
@@ -0,0 +1,26 @@
+function bruteVer(nums: number[], target: number): number[] | void {
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) return [i, j];
+    }
+  }
+}
+
+function hashVer(nums: number[], target: number): number[] | void {
+  const dict: Record<number, number> = {};
+
+  for (let [index, val] of nums.entries()) {
+    const lookupIndex = dict[target - val];
+    if (lookupIndex !== undefined) return [lookupIndex, index];
+    dict[val] = index;
+  }
+}
+
+const testNums = [-1, 2, 11, 15, 7];
+const testTarget = 9;
+
+const bruteResult = bruteVer(testNums, testTarget);
+console.log('bruteResult', bruteResult);
+
+const hashResult = hashVer(testNums, testTarget);
+console.log('hashVer', hashResult);
