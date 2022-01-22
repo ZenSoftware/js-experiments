7e6f75a7af3a207618d967549ebc7d075da36e45
Zen Software <ZenSoftware@users.noreply.github.com>
2022-01-22 02:49:38 -0700

    renamed descriptions

--- leet/two-sum.spec.ts
+++ leet/two-sum.spec.ts
@@ -1,12 +1,12 @@
 import { twoSumHash, twoSumBrute } from './two-sum';
 
-test('brute version leetcode test', () => {
+test('leetcode test brute version', () => {
   expect(twoSumBrute([2, 7, 11, 15], 9)).toEqual([0, 1]);
   expect(twoSumBrute([3, 2, 4], 6)).toEqual([1, 2]);
   expect(twoSumBrute([3, 3], 6)).toEqual([0, 1]);
 });
 
-test('hash version leetcode test', () => {
+test('leetcode test for hash version', () => {
   expect(twoSumHash([2, 7, 11, 15], 9)).toEqual([0, 1]);
   expect(twoSumHash([3, 2, 4], 6)).toEqual([1, 2]);
   expect(twoSumHash([3, 3], 6)).toEqual([0, 1]);
