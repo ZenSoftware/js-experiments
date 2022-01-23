b7d66fd50cd9944954699f00a5bee2ab302f2ca9
Zen Software <ZenSoftware@users.noreply.github.com>
2022-01-22 17:51:58 -0700

    working

--- leet/longest-substring.spec.ts
+++ leet/longest-substring.spec.ts
@@ -5,12 +5,17 @@ test('leetcode test 1', () => {
   expect(result).toBe(3);
 });
 
+test('leetcode test 3', () => {
+  const result = lengthOfLongestSubstring('pwwkew');
+  expect(result).toBe(3);
+});
+
 test('leetcode test 2', () => {
   const result = lengthOfLongestSubstring('bbbbb');
   expect(result).toBe(1);
 });
 
-test('leetcode test 3', () => {
-  const result = lengthOfLongestSubstring('pwwkew');
-  expect(result).toBe(3);
+test('leetcode test 4', () => {
+  const result = lengthOfLongestSubstring('');
+  expect(result).toBe(0);
 });
--- leet/longest-substring.ts
+++ leet/longest-substring.ts
@@ -1,11 +1,32 @@
 /** https://leetcode.com/problems/longest-substring-without-repeating-characters/ */
 
+// export function almost(s: string): number {
+//   let longest = '';
+
+//   for (let start = 0; start < s.length; start++) {
+//     for (let end = start + 1; end < s.length && s.length - start > longest.length; end++) {
+//       console.log(`${s.substring(start, end)} [${s[end]}]`);
+
+//       if (s.substring(start, end).includes(s[end])) {
+//         break;
+//       }
+
+//       if (1 + end - start > longest.length) {
+//         longest = s.substring(start, end + 1);
+//       }
+//     }
+//   }
+
+//   console.log('LONGEST:', longest);
+//   return longest.length;
+// }
+
 export function lengthOfLongestSubstring(s: string): number {
   let longest = '';
 
   for (let start = 0; start < s.length; start++) {
     let evaluate = '';
-    for (let end = start + 1; end <= s.length && s.length - start > longest.length; end++) {
+    for (let end = start + 1; end <= s.length; end++) {
       if (evaluate.includes(s[end - 1])) {
         break;
       }
@@ -23,5 +44,5 @@ export function lengthOfLongestSubstring(s: string): number {
   return longest.length;
 }
 
-const result = lengthOfLongestSubstring('12344444');
+const result = lengthOfLongestSubstring('123345');
 console.log('RESULT:', result);
