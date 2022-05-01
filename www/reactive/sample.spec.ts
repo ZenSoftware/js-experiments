83a6d97991ad29745f3ef1f28daebae155d7ba68
Zen Software <ZenSoftware@users.noreply.github.com>
2022-05-01 04:49:18 -0600

    refactored structure

--- www/reactive/sample.spec.ts
+++ www/reactive/sample.spec.ts
@@ -1,11 +1,27 @@
 import {} from './sample';
 import { subscribeSpyTo } from '@hirez_io/observer-spy';
-import { of, take } from 'rxjs';
+import { of, take, throwError } from 'rxjs';
 
-describe('sample', () => {
-  it('should work', () => {
-    const first$ = of(1, 2, 3).pipe(take(1));
-    const observerSpy = subscribeSpyTo(first$);
-    expect(observerSpy.getValues()).toEqual([1]);
+describe('observer-spy', () => {
+  it('should have all demos work', () => {
+    const fake$ = of('first', 'second', 'third');
+    const observerSpy = subscribeSpyTo(fake$);
+
+    expect(observerSpy.getFirstValue()).toEqual('first');
+    expect(observerSpy.receivedNext()).toEqual(true);
+    expect(observerSpy.getValues()).toEqual(['first', 'second', 'third']);
+    expect(observerSpy.getValuesLength()).toEqual(3);
+    expect(observerSpy.getFirstValue()).toEqual('first');
+    expect(observerSpy.getValueAt(1)).toEqual('second');
+    expect(observerSpy.getLastValue()).toEqual('third');
+    expect(observerSpy.receivedComplete()).toEqual(true);
+  });
+
+  it('should error', () => {
+    const fake$ = throwError(() => new Error('spy error'));
+    const observerSpy = subscribeSpyTo(fake$, { expectErrors: true });
+
+    expect(observerSpy.receivedError()).toEqual(true);
+    expect(observerSpy.getError()).toEqual(new Error('spy error'));
   });
 });
