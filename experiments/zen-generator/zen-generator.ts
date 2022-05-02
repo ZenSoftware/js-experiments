a442586d1b724560b5169217820e4a334df944d2
Zen Software <ZenSoftware@users.noreply.github.com>
2022-05-02 15:52:31 -0600

    itterators

--- experiments/zen-generator/zen-generator.ts
+++ experiments/zen-generator/zen-generator.ts
@@ -2,4 +2,13 @@ export function* zenG(amount: number) {
   for (let i = 1; i <= amount; i++) {
     yield i;
   }
+  yield* [8, 9];
+}
+
+export function* getId() {
+  let i = 1;
+  while (true) {
+    let ret: number | undefined = yield i++;
+    if (ret !== undefined) i += ret;
+  }
 }
--- experiments/zen-generator/zen-generators.spec.ts
+++ experiments/zen-generator/zen-generators.spec.ts
@@ -1,4 +1,4 @@
-import { zenG } from './zen-generator';
+import { zenG, getId } from './zen-generator';
 
 describe('zenG', () => {
   it('should yield correctly', () => {
@@ -8,6 +8,14 @@ describe('zenG', () => {
       result.push(z);
     }
 
-    expect(result).toEqual([1, 2, 3, 4, 5]);
+    expect(result).toEqual([1, 2, 3, 4, 5, 8, 9]);
+  });
+
+  it('should yield ids', () => {
+    const itter = getId();
+    expect(itter.next().value).toEqual(1);
+    expect(itter.next(5).value).toEqual(7);
+    expect(itter.next().value).toEqual(8);
+    expect(itter.next().value).toEqual(9);
   });
 });
