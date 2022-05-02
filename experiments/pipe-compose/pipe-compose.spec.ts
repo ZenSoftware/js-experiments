f398712e9dca60feee364dc09b8b4f1695661ab1
Zen Software <ZenSoftware@users.noreply.github.com>
2022-05-02 16:21:37 -0600

    abbreviated descriptions

--- experiments/pipe-compose/pipe-compose.spec.ts
+++ experiments/pipe-compose/pipe-compose.spec.ts
@@ -1,7 +1,7 @@
 import { compose, pipe } from './pipe-compose';
 
 describe('compose', () => {
-  it('should execute left to right', () => {
+  it('executes left to right', () => {
     const add5 = x => x + 5;
     const multiply2 = x => x * 2;
     const subtract1 = x => x - 1;
@@ -12,7 +12,7 @@ describe('compose', () => {
 });
 
 describe('pipe', () => {
-  it('should execute right to left', () => {
+  it('executes right to left', () => {
     const add5 = x => x + 5;
     const multiply2 = x => x * 2;
     const subtract1 = x => x - 1;
