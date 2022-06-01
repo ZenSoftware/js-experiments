07c400b5ccff20e2a7e9f9a3e9a2ae41c8afceea
Zen Software <ZenSoftware@users.noreply.github.com>
2022-05-31 20:01:21 -0600

    fibonacci

--- www/webworkers/script.ts
+++ www/webworkers/script.ts
@@ -1,7 +1,20 @@
-import { fromEvent } from 'rxjs';
+const startFibButton = document.querySelector('#startFibButton') as HTMLButtonElement;
+const spinner = document.querySelector('#spinner') as HTMLElement;
+const resultContainer = document.querySelector('#resultContainer') as HTMLPreElement;
 
-const click$ = fromEvent<MouseEvent>(document, 'click');
+function showLoading(show: boolean) {
+  if (show) spinner.removeAttribute('hidden');
+  else spinner.setAttribute('hidden', '');
+}
 
-click$.subscribe(e => {
-  console.log(`Click: ${e.offsetX}, ${e.offsetY}`);
+startFibButton?.addEventListener('mousedown', () => {
+  showLoading(true);
+  const result = fib(45);
+  resultContainer.innerHTML = result.toString();
+  showLoading(false);
 });
+
+function fib(n) {
+  if (n === 1 || n === 2) return 1;
+  return fib(n - 1) + fib(n - 2);
+}
