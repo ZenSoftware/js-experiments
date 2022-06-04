a301e34fca9e64ef581e8d593095a7b5de38c4a8
Zen Software <ZenSoftware@users.noreply.github.com>
2022-06-04 00:38:11 -0600

    can-construct itterative

--- experiments/can-construct/can-construct.ts
+++ experiments/can-construct/can-construct.ts
@@ -1,4 +1,6 @@
-function canConstruct(target: string, wordBank: string[]) {}
+// function canConstruct(target: string, wordBank: string[]) {
+
+// }
 
 console.log(canConstruct('shabadaba', ['da', 'sha', 'ba']));
 console.log(canConstruct('shabadaba', ['ku', 'da', 'sha', 'ba']));
@@ -8,21 +10,22 @@ console.log(canConstruct('shabadaba', ['ku', 'da', 'ba']));
 console.log(canConstruct('shabadaba', ['ku', 'sha', 'ba']));
 console.log(canConstruct('shabadaba', []));
 
-// function canConstruct(target: string, wordBank: string[]) {
-//   let accum = '';
+function canConstruct(target: string, wordBank: string[]) {
+  let accum = '';
 
-//   wordBank = wordBank.filter(w => w !== '');
+  wordBank = wordBank.filter(w => w !== '');
 
-//   while (accum.length < target.length) {
-//     let added = false;
-//     for (let i = 0; i < wordBank.length; i++) {
-//       if (target.startsWith(accum + wordBank[i])) {
-//         accum += wordBank[i];
-//         added = true;
-//         break;
-//       }
-//     }
-//     if (!added) return false;
-//   }
-//   return true;
-// }
+  while (accum.length < target.length) {
+    let added = false;
+    for (let word of wordBank) {
+      if (target.startsWith(accum + word)) {
+        accum += word;
+        added = true;
+        break;
+      }
+    }
+
+    if (!added) return false;
+  }
+  return true;
+}
