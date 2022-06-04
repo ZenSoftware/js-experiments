e7b2f12b52e0f9046431d31075beb193833c3a04
Zen Software <ZenSoftware@users.noreply.github.com>
2022-06-04 08:25:27 -0600

    docs

--- experiments/can-sum/best-sum-tabular.ts
+++ experiments/can-sum/best-sum-tabular.ts
@@ -30,6 +30,11 @@ function bestSum(targetSum: number, numbers: number[]) {
   return optimal;
 }
 
+/**
+ * Time: O(m^2*m)
+ * Space: O(m^2)
+ */
+
 console.log(bestSum(11, [2, 5, 6]));
 console.log(bestSum(8, [2, 3, 5]));
 console.log(bestSum(8, [7, 5, 3, 2]));
