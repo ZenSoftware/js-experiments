bdcaa7ca65c9e207146f5bdabcd1561443a84ebd
Zen Software <ZenSoftware@users.noreply.github.com>
2022-06-04 14:01:13 -0600

    count-bits

---
+++ leet/count-bits.ts
@@ -0,0 +1,24 @@
+/**
+ * https://leetcode.com/problems/counting-bits/
+ * Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n),
+ * ans[i] is the number of 1's in the binary representation of i.
+ */
+
+function countBits(n: number): number[] {
+  const result: number[] = Array(n + 1);
+  for (let i = 0; i <= n; i++) {
+    let temp = i;
+    let count = 0;
+    while (temp > 0) {
+      if (temp & 1) count++;
+      temp = temp >> 1;
+    }
+    result[i] = count;
+  }
+
+  return result;
+}
+
+console.time('bench');
+console.log(countBits(5));
+console.timeEnd('bench');
