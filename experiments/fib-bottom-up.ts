5db82f840586f3d1b05e9f9c8dbc87c6b0538b2e
Zen Software <ZenSoftware@users.noreply.github.com>
2022-06-04 04:31:39 -0600

    fib-bottom-up done

--- experiments/fib-bottom-up.ts
+++ experiments/fib-bottom-up.ts
@@ -1,8 +1,10 @@
 function fib(n: number) {
-  const table = [0, 1];
+  const table = Array(n + 2).fill(0);
+  table[1] = 1;
 
-  for (let i = 2; i <= n; i++) {
-    table.push(table[i - 1] + table[i - 2]);
+  for (let i = 0; i < n; i++) {
+    table[i + 1] += table[i];
+    table[i + 2] += table[i];
   }
 
   console.log(table);
