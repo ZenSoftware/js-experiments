d4ad72079fe271162e1dc555ef4876e22cb89276
Zen Software <ZenSoftware@users.noreply.github.com>
2022-06-04 05:14:57 -0600

    cleaned up

--- experiments/grid-traveller/grid-traveller.ts
+++ experiments/grid-traveller/grid-traveller.ts
@@ -4,19 +4,19 @@
  * Count the number of ways to arrive at the bottom left
  */
 
-export function gridTraveller(m: number, n: number) {
-  const memo: { [key: string]: any } = {};
+function gridTraveller(m: number, n: number) {
+  const memo = {};
 
-  const compute = (_m: number, _n: number) => {
-    const key = `${_m}-${_n}`;
-    let cache = memo[key];
-    if (cache === undefined) cache = memo[`${_n}-${_m}`];
-    if (cache !== undefined) return cache;
+  const compute = (row: number, col: number) => {
+    const key = `${row}-${col}`;
+    if (key in memo) return memo[key];
+    if (`${col}-${row}` in memo) return memo[`${col}-${row}`];
 
-    if (_m === 0 || _n === 0) return 0;
-    if (_m === 1 && _n === 1) return 1;
+    if (row === 0 || col === 0) return 0;
+    if (row === 1 && col === 1) return 1;
 
-    const result = compute(_m - 1, _n) + compute(_m, _n - 1);
+    const result = compute(row - 1, col) + compute(row, col - 1);
+
     memo[key] = result;
     return result;
   };
