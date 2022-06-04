336afed7dce7e27deb385dba542a90a6f6bc9447
Zen Software <ZenSoftware@users.noreply.github.com>
2022-06-04 08:23:05 -0600

    improved space complexity

--- experiments/can-sum/best-sum-tabular.ts
+++ experiments/can-sum/best-sum-tabular.ts
@@ -8,20 +8,28 @@ function bestSum(targetSum: number, numbers: number[]) {
   const table = Array(targetSum + 1).fill(null);
 
   table[0] = [];
+  let optimal = null;
 
   for (let i = 0; i <= targetSum; i++) {
     if (table[i]) {
       for (let num of numbers) {
-        if (i + num <= targetSum) {
-          table[i + num] = [...table[i], num];
+        const numIndex = i + num;
+        if (numIndex <= targetSum) {
+          table[numIndex] = [...table[i], num];
+
+          if (numIndex === targetSum) {
+            if (optimal === null || table[targetSum].length < optimal.length) {
+              optimal = table[targetSum];
+            }
+          }
         }
       }
     }
   }
 
-  return table[targetSum];
+  return optimal;
 }
 
 console.log(bestSum(11, [2, 5, 6]));
 console.log(bestSum(8, [2, 3, 5]));
-console.log(bestSum(8, [1, 4, 5]));
+console.log(bestSum(8, [7, 5, 3, 2]));
