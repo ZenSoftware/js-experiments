cc9358230c6549690fabb4c090e5c1165e97265c
Zen Software <ZenSoftware@users.noreply.github.com>
2022-06-04 04:41:08 -0600

    more efficient space complexity example

--- experiments/fib-bottom-up.ts
+++ experiments/fib-bottom-up.ts
@@ -2,6 +2,9 @@ function fib(n: number) {
   const table = Array(n + 2).fill(0);
   table[1] = 1;
 
+  // for (let i = 0; i <= n; i++) {
+  //   table[i + 2] = table[i] + table[i + 1];
+  // }
   for (let i = 0; i < n; i++) {
     table[i + 1] += table[i];
     table[i + 2] += table[i];
@@ -11,4 +14,18 @@ function fib(n: number) {
   return table[n];
 }
 
-console.log(fib(6));
+// function fib(n: number) {
+//   let prev1 = 1;
+//   let prev2 = 1;
+
+//   for (let i = 1; i < n; i++) {
+//     const temp1 = prev1;
+//     const temp2 = prev2;
+//     prev2 = temp1 + temp2;
+//     prev1 = temp2;
+//   }
+
+//   return prev1;
+// }
+
+console.log(fib(7));
