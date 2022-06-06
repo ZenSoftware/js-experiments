af047f3cfa01f228ac7d662583a9e411494e0799
Zen Software <ZenSoftware@users.noreply.github.com>
2022-06-06 11:17:35 -0600

    corrected bounds

--- leet/3sum/3sum.ts
+++ leet/3sum/3sum.ts
@@ -4,8 +4,8 @@
 function threeSum(nums: number[]): number[][] {
   const result: number[][] = [];
 
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
+  for (let i = 0; i < nums.length - 2; i++) {
+    for (let j = i + 1; j < nums.length - 1; j++) {
       for (let k = j + 1; k < nums.length; k++) {
         if (nums[i] + nums[j] + nums[k] === 0) {
           const sum0 = [nums[i], nums[j], nums[k]].sort();
