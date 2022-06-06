56847cdd20f800ed2270b335d3d7d8e18cb9ec8c
Zen Software <ZenSoftware@users.noreply.github.com>
2022-06-06 07:59:38 -0600

    longest-common-prefix skeleton

---
+++ leet/longest-common-prefix/longest-common-prefix.ts
@@ -0,0 +1,10 @@
+/**
+ * https://leetcode.com/problems/longest-common-prefix/
+ * Write a function to find the longest common prefix string amongst an array of strings.
+ * If there is no common prefix, return an empty string "".
+ */
+function longestCommonPrefix(strs: string[]): string {
+  return '';
+}
+
+console.log(longestCommonPrefix(['flower', 'flow', 'flight']));
