0459cfe97063271f4d5bdd8686f54bcb2f871add
Zen Software <ZenSoftware@users.noreply.github.com>
2022-06-06 08:44:38 -0600

    validate

--- leet/longest-common-prefix/longest-common-prefix.ts
+++ leet/longest-common-prefix/longest-common-prefix.ts
@@ -5,10 +5,26 @@
  */
 function longestCommonPrefix(strs: string[]): string {
   if (strs.length === 1) return strs[0];
+  if (!hasCommonPrefix(strs)) return '';
 
   let result = '';
 
   return result;
 }
 
+function hasCommonPrefix(strs: string[]) {
+  const firstLetters: string[] = [];
+
+  for (let i = 0; i < strs.length; i++) {
+    firstLetters.push(strs[i][0]);
+  }
+
+  for (let letter of firstLetters) {
+    if (firstLetters.indexOf(letter) !== firstLetters.lastIndexOf(letter)) return true;
+  }
+
+  return false;
+}
+
 console.log(longestCommonPrefix(['flower', 'flow', 'flight']));
+console.log(longestCommonPrefix(['flower', 'flow', 'abcde', 'abcdef']));
