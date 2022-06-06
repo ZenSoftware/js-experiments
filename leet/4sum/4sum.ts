accada1620a4b59d9506c6af23ddb03936e0dd44
Zen Software <ZenSoftware@users.noreply.github.com>
2022-06-06 13:07:10 -0600

    4sum skeleton

---
+++ leet/4sum/4sum.ts
@@ -0,0 +1,20 @@
+/**
+ * https://leetcode.com/problems/4sum/
+ * Given an array nums of n integers, return an array of all the
+ * unique quadruplets [nums[a], nums[b], nums[c], nums[d]] such that:
+ *
+ * - 0 <= a, b, c, d < n
+ * - a, b, c, and d are distinct.
+ * - nums[a] + nums[b] + nums[c] + nums[d] == target
+ *
+ * You may return the answer in any order.
+ */
+function fourSum(nums: number[], target: number): number[][] {
+  if (nums.length < 4) return [];
+
+  const result: number[][] = [];
+
+  return result;
+}
+
+console.log(fourSum([1, 0, -1, 0, -2, 2], 0));
