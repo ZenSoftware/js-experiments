c5a98f796f0526c388341226d79866ce03740251
Zen Software <ZenSoftware@users.noreply.github.com>
2022-06-06 06:09:48 -0600

    O(n) algorithm

--- leet/container-with-most-water/container-with-most-water.ts
+++ leet/container-with-most-water/container-with-most-water.ts
@@ -6,23 +6,32 @@
  * Find two lines that together with the x-axis form a container, such that the container contains the most water.
  *
  * Return the maximum amount of water a container can store.
+ *
+ * An explination to the O(n) solution can be found here:
+ * https://leetcode.com/problems/container-with-most-water/discuss/6099/yet-another-way-to-see-what-happens-in-the-on-algorithm
  */
 function maxArea(height: number[]): number {
   let maxResult = 0;
 
-  const dfs = (start: number) => {
-    for (let end = start + 1; end < height.length; end++) {
-      const h = Math.min(height[start], height[end]);
-      let w = end - start;
-      const area = w * h;
-      if (area > maxResult) maxResult = area;
-      dfs(end);
-    }
-  };
+  let left = 0;
+  let right = height.length - 1;
+  while (left < right) {
+    const width = right - left;
 
-  dfs(0);
+    let area: number;
+    if (height[left] < height[right]) {
+      area = height[left] * width;
+      ++left;
+    } else {
+      area = height[right] * width;
+      --right;
+    }
+
+    if (area > maxResult) maxResult = area;
+  }
+
   return maxResult;
 }
 
-const height = [1, 1, 3];
+const height = [1, 8, 6, 2, 5, 4, 8, 3, 7];
 console.log(maxArea(height));
