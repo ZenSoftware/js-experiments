354c2635a77abc6ae7cf29fcd43512a4e5b4386b
Zen Software <ZenSoftware@users.noreply.github.com>
2022-06-06 12:55:09 -0600

    solution done

--- leet/remove-nth-from-end/remove-nth-from-end.ts
+++ leet/remove-nth-from-end/remove-nth-from-end.ts
@@ -14,7 +14,27 @@ class ListNode {
 }
 
 function removeNthFromEnd(head: ListNode | null, n: number): ListNode | null {
-  return null;
+  if ((<ListNode>head).next === null) return null;
+
+  let list: ListNode[] = [];
+  let next = head as ListNode;
+  list.push(next);
+
+  while (next.next !== null) {
+    next = next.next as ListNode;
+    list.push(next);
+  }
+
+  const index = list.length - 1 - n;
+  list[index].next = list[index + 1].next;
+
+  next = head as ListNode;
+  while (next.next !== null) {
+    next = next.next as ListNode;
+    // console.log(next.val);
+  }
+
+  return head;
 }
 
 const node1 = new ListNode(1);
@@ -28,4 +48,4 @@ node2.next = node3;
 node3.next = node4;
 node4.next = node5;
 
-removeNthFromEnd(node1, 2);
+console.log(removeNthFromEnd(node1, 2));
