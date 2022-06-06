e096a36948e6377dedc68dfa38841c5249a29e9c
Zen Software <ZenSoftware@users.noreply.github.com>
2022-06-06 12:24:16 -0600

    remove-nth-from-end skeleton

--- leet/remove-nth-from-end/remove-nth-from-end.ts
+++ leet/remove-nth-from-end/remove-nth-from-end.ts
@@ -13,4 +13,19 @@ class ListNode {
   }
 }
 
-function removeNthFromEnd(head: ListNode | null, n: number): ListNode | null {}
+function removeNthFromEnd(head: ListNode | null, n: number): ListNode | null {
+  return null;
+}
+
+const node1 = new ListNode(1);
+const node2 = new ListNode(2);
+const node3 = new ListNode(3);
+const node4 = new ListNode(4);
+const node5 = new ListNode(5);
+
+node1.next = node2;
+node2.next = node3;
+node3.next = node4;
+node4.next = node5;
+
+removeNthFromEnd(node1, 2);
