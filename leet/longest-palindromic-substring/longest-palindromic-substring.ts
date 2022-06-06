44f56e74223fb7196fcb1858ae908be680bf1eca
Zen Software <ZenSoftware@users.noreply.github.com>
2022-06-06 00:38:20 -0600

    expand for center

--- leet/longest-palindromic-substring/longest-palindromic-substring.ts
+++ leet/longest-palindromic-substring/longest-palindromic-substring.ts
@@ -9,37 +9,33 @@ export function longestPalindrome(s: string) {
   let optimal = '';
 
   for (let i = 0; i < s.length; i++) {
-    let largestSub = s.charAt(i);
     // expand from center
-    let start = i - 1;
-    let end = i + 1;
-    while (start >= 0 && end < s.length) {
-      if (s.charAt(start) !== s.charAt(end)) {
-        break;
-      }
-      largestSub = s.slice(start, end + 1);
-      --start;
-      ++end;
+    const largestSubOdd = expandFromCenter(s, i - 1, i + 1);
+    if (largestSubOdd.length > optimal.length) {
+      optimal = largestSubOdd;
     }
 
-    // replace optimal if substring is larger
-    if (largestSub.length > optimal.length) {
-      optimal = largestSub;
+    const largestSubEven = expandFromCenter(s, i, i + 1);
+    if (largestSubEven.length > optimal.length) {
+      optimal = largestSubEven;
     }
   }
 
-  return optimal;
+  if (optimal === '') return s.charAt(0);
+  else return optimal;
 }
 
-export function isPalindrome(str: string) {
-  let start = 0;
-  let end = str.length - 1;
-  while (start < end) {
-    if (str.charAt(start++) !== str.charAt(end--)) {
-      return false;
+function expandFromCenter(s: string, start: number, end: number) {
+  let largestSub = '';
+  while (start >= 0 && end < s.length) {
+    if (s.charAt(start) !== s.charAt(end)) {
+      break;
     }
+    largestSub = s.slice(start, end + 1);
+    --start;
+    ++end;
   }
-  return true;
+  return largestSub;
 }
 
-console.log(longestPalindrome('abacddc'));
+console.log(longestPalindrome('abcba'));
