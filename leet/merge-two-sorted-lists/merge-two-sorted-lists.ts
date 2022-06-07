4ba9f95c71738c310129fa308aa4763816421951
Zen Software <ZenSoftware@users.noreply.github.com>
2022-06-07 00:06:27 -0600

    work

---
+++ leet/merge-two-sorted-lists/merge-two-sorted-lists.ts
@@ -0,0 +1,63 @@
+/**
+ * https://leetcode.com/problems/merge-two-sorted-lists/
+ * You are given the heads of two sorted linked lists list1 and list2.
+ *
+ * Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+ *
+ * Return the head of the merged linked list.
+ */
+
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
+  if (list1 === null && list2 === null) return null;
+  else if (list2 === null) return list1;
+  else if (list1 === null) return list2;
+
+  const listResult = list1.val < list2.val ? list1 : list2;
+  let nextResult = listResult;
+  let next1 = list1;
+  let next2 = list2;
+  while (next1.next !== null || next2.next !== null) {
+    if (next1.next === null) {
+      nextResult.next = next2;
+      return listResult;
+    } else if (next2.next === null) {
+      nextResult.next = next1;
+      return listResult;
+    }
+
+    if (list1.val < list2.val) {
+    } else {
+    }
+  }
+
+  return listResult;
+}
+
+const nodeA1 = new ListNode(1);
+const nodeA2 = new ListNode(2);
+const nodeA3 = new ListNode(4);
+nodeA1.next = nodeA2;
+nodeA2.next = nodeA3;
+
+const nodeB1 = new ListNode(1);
+const nodeB2 = new ListNode(3);
+const nodeB3 = new ListNode(4);
+nodeB1.next = nodeB2;
+nodeB2.next = nodeB3;
+
+// const merged = mergeTwoLists(nodeA1, nodeB1);
+// console.log(getValues(merged));
+
+function getValues(root: ListNode | null): number[] {
+  if (root === null) return [];
+  return [root.val, ...getValues(root.next)];
+}
