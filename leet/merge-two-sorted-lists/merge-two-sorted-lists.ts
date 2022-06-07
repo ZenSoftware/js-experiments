1dbbe2f79bef06166b986a1d689631a5fdbfb020
Zen Software <ZenSoftware@users.noreply.github.com>
2022-06-07 11:45:45 -0600

    merge-two-lists skeleton

--- leet/merge-two-sorted-lists/merge-two-sorted-lists.ts
+++ leet/merge-two-sorted-lists/merge-two-sorted-lists.ts
@@ -21,25 +21,9 @@ function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode
   else if (list2 === null) return list1;
   else if (list1 === null) return list2;
 
-  const listResult = list1.val < list2.val ? list1 : list2;
-  let nextResult = listResult;
-  let next1 = list1;
-  let next2 = list2;
-  while (next1.next !== null || next2.next !== null) {
-    if (next1.next === null) {
-      nextResult.next = next2;
-      return listResult;
-    } else if (next2.next === null) {
-      nextResult.next = next1;
-      return listResult;
-    }
+  const head = list1.val < list2.val ? list1 : list2;
 
-    if (list1.val < list2.val) {
-    } else {
-    }
-  }
-
-  return listResult;
+  return head;
 }
 
 const nodeA1 = new ListNode(1);
@@ -54,8 +38,8 @@ const nodeB3 = new ListNode(4);
 nodeB1.next = nodeB2;
 nodeB2.next = nodeB3;
 
-// const merged = mergeTwoLists(nodeA1, nodeB1);
-// console.log(getValues(merged));
+const merged = mergeTwoLists(nodeA1, nodeB1);
+console.log(getValues(merged));
 
 function getValues(root: ListNode | null): number[] {
   if (root === null) return [];
