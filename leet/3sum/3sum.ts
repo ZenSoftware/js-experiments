792b60c66c38e8b4d6ca8c3d8b54997f7ad4c313
Zen Software <ZenSoftware@users.noreply.github.com>
2022-06-06 21:27:44 -0600

    solution

--- leet/3sum/3sum.ts
+++ leet/3sum/3sum.ts
@@ -8,7 +8,7 @@ function threeSum(nums: number[]): number[][] {
     for (let j = i + 1; j < nums.length - 1; j++) {
       for (let k = j + 1; k < nums.length; k++) {
         if (nums[i] + nums[j] + nums[k] === 0) {
-          const sum0 = [nums[i], nums[j], nums[k]].sort();
+          const sum0 = [nums[i], nums[j], nums[k]].sort(x => x);
           if (!resultContains(result, sum0)) {
             result.push(sum0);
           }
--- leet/4sum/4sum.ts
+++ leet/4sum/4sum.ts
@@ -14,6 +14,18 @@ function fourSum(nums: number[], target: number): number[][] {
 
   const result: number[][] = [];
 
+  for (let i = 0; i < nums.length - 3; i++) {
+    for (let j = i + 1; j < nums.length - 2; j++) {
+      for (let k = j + 1; k < nums.length - 1; k++) {
+        for (let l = k + 1; l < nums.length; l++) {
+          if (nums[i] + nums[j] + nums[k] + nums[l] === target) {
+            result.push([nums[i], nums[j], nums[k], nums[l]].sort(x => x));
+          }
+        }
+      }
+    }
+  }
+
   return result;
 }
 
