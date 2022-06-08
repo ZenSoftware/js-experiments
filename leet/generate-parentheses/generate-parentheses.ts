0debb8b232a1044afd2dcd67c895f472ce74983b
Zen Software <ZenSoftware@users.noreply.github.com>
2022-06-08 08:54:36 -0600

    generate-parentheses skeleton

---
+++ leet/generate-parentheses/generate-parentheses.ts
@@ -0,0 +1,9 @@
+/**
+ * https://leetcode.com/problems/generate-parentheses/
+ * Given n pairs of parentheses, write a function to generate
+ * all combinations of well-formed parentheses.
+ */
+
+function generateParenthesis(n: number): string[] {}
+
+console.log(generateParenthesis(3));
