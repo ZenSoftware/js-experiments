12ba3604f0634d933d94828647e451b7a41b1c04
Zen Software <ZenSoftware@users.noreply.github.com>
2022-06-08 10:50:15 -0600

    solution

--- leet/generate-parentheses/generate-parentheses.ts
+++ leet/generate-parentheses/generate-parentheses.ts
@@ -4,6 +4,38 @@
  * all combinations of well-formed parentheses.
  */
 
-function generateParenthesis(n: number): string[] {}
+function generateParenthesis(n: number): string[] {
+  const combinations: string[] = [];
+
+  let current: string[] = [];
+  for (let i = 1; i <= 2 * n; i++) {
+    current.push('');
+  }
+
+  generateAll(current, 0, combinations);
+  return combinations;
+}
+
+function generateAll(current: string[], pos: number, result: string[]) {
+  if (pos == current.length) {
+    if (valid(current)) result.push(current.join(''));
+  } else {
+    current[pos] = '(';
+    generateAll(current, pos + 1, result);
+    current[pos] = ')';
+    generateAll(current, pos + 1, result);
+  }
+}
+
+function valid(current: string[]) {
+  let balance = 0;
+  for (let c of current) {
+    if (c == '(') balance++;
+    else balance--;
+    if (balance < 0) return false;
+  }
+
+  return balance == 0;
+}
 
 console.log(generateParenthesis(3));
