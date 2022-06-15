52cf474ad4f7ad112816d865ebc626eb484d5236
Zen Software <ZenSoftware@users.noreply.github.com>
2022-06-15 13:07:56 -0600

    skeleton

---
+++ leet/merge-k-lists/merge-k-lists.ts
@@ -0,0 +1,24 @@
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+function mergeKLists(lists: Array<ListNode | null>): ListNode | null {}
+
+const node1 = new ListNode(1);
+const node2 = new ListNode(2);
+const node3 = new ListNode(3);
+
+node1.next = node2;
+node2.next = node3;
+
+console.log(getVals(node1));
+
+function getVals(root: ListNode) {
+  if (root === null) return [];
+  return [root.val, ...getVals(root.next as ListNode)];
+}
