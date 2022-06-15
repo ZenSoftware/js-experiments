0eee057bc4a1043000d342b4ad5dcd4b91112930
Zen Software <ZenSoftware@users.noreply.github.com>
2022-06-15 13:08:27 -0600

    skeleton docs

--- leet/merge-k-lists/merge-k-lists.ts
+++ leet/merge-k-lists/merge-k-lists.ts
@@ -1,3 +1,8 @@
+/**
+ * https://leetcode.com/problems/merge-k-sorted-lists/
+ * You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+ * Merge all the linked-lists into one sorted linked-list and return it.
+ */
 class ListNode {
   val: number;
   next: ListNode | null;
