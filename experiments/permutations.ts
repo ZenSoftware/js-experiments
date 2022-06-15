311574aa86cb6ba14121e060a364c843c8190a77
Zen Software <ZenSoftware@users.noreply.github.com>
2022-06-15 07:29:18 -0600

    permutations skeleton

---
+++ experiments/permutations.ts
@@ -0,0 +1,11 @@
+/**
+ * https://www.youtube.com/watch?v=us0cYQXQpxg
+ * ## Complexity
+ * Time: O()
+ * Space: O()
+ */
+
+function permutations(elements: any[]) {}
+
+const sample = ['a', 'b', 'c'];
+console.log(permutations(sample));
