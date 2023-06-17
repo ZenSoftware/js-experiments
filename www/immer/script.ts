df7e2f600da4a6a5c0d5b890ddb678b9256235d5
Zen Software <ZenSoftware@users.noreply.github.com>
2023-06-16 22:14:13 -0600

    show example of trying to mutate frozen obj

--- www/immer/script.ts
+++ www/immer/script.ts
@@ -2,7 +2,7 @@ import { produce, enableMapSet } from 'immer';
 
 enableMapSet();
 
-const usersById_v1 = new Map([['michel', { name: 'Michel Weststrate', country: 'NL' }]]);
+const usersById_v1 = new Map([['sam', { name: 'Sam Harris', country: 'US' }]]);
 
 const usersById_v2 = produce(usersById_v1, draft => {
   const user = draft.get('sam');
@@ -10,3 +10,15 @@ const usersById_v2 = produce(usersById_v1, draft => {
     user.country = 'UK';
   }
 });
+
+console.log(usersById_v2);
+
+console.log(usersById_v1);
+
+/**
+ * This will throw an error because the draft is read-only
+ */
+// const sam = usersById_v2.get('sam');
+// if(sam) {
+//   sam.country = 'CA';
+// }
