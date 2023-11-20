e9257a8f90f08b9f5be644c8432bdf678ac89dac
Zen Software <ZenSoftware@users.noreply.github.com>
2023-11-20 12:41:02 -0700

    docs update

--- leet-microsoft/add-two-numbers/add-two-numbers.ts
+++ leet-microsoft/add-two-numbers/add-two-numbers.ts
@@ -2,6 +2,9 @@
  * https://leetcode.com/problems/add-two-numbers/?envType=featured-list&envId=top-microsoft-questions?envType=featured-list&envId=top-microsoft-questions
  * You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
  * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ * - The number of nodes in each linked list is in the range [1, 100].
+ * - 0 <= Node.val <= 9
+ * - It is guaranteed that the list represents a number that does not have leading zeros.
  */
 
 /**
