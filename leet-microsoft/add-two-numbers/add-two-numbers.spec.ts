7ac618fe6f16fff854582158a8323d93487577dd
Zen Software <ZenSoftware@users.noreply.github.com>
2023-11-20 12:32:57 -0700

    test edge case

--- leet-microsoft/add-two-numbers/add-two-numbers.spec.ts
+++ leet-microsoft/add-two-numbers/add-two-numbers.spec.ts
@@ -5,6 +5,10 @@ describe('Two Sum', () => {
     const list = toList([1, 2, 3]);
     const array = toArray(list);
     expect(array).toEqual([1, 2, 3]);
+
+    const list2 = toList([1]);
+    const array2 = toArray(list2);
+    expect(array2).toEqual([1]);
   });
 
   it('evaluates correctly', () => {});
