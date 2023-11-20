7abb1424cbbce11519d6af2372ac1997f4cce84e
Zen Software <ZenSoftware@users.noreply.github.com>
2023-11-20 15:34:21 -0700

    working solution

--- leet-microsoft/longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.spec.ts
+++ leet-microsoft/longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.spec.ts
@@ -2,8 +2,10 @@ import { lengthOfLongestSubstring } from './longest-substring-without-repeating-
 
 describe('Longest Substring Without Repeating Characters', () => {
   it('evaluates correcty', () => {
-    expect(lengthOfLongestSubstring('abcabcbb')).toEqual(3);
-    expect(lengthOfLongestSubstring('bbbbb')).toEqual(1);
+    // expect(lengthOfLongestSubstring('')).toEqual(0);
+    // expect(lengthOfLongestSubstring('a')).toEqual(1);
+    // expect(lengthOfLongestSubstring('abcabcbb')).toEqual(3);
+    // expect(lengthOfLongestSubstring('bbbbb')).toEqual(1);
     expect(lengthOfLongestSubstring('pwwkew')).toEqual(3);
   });
 });
--- leet-microsoft/longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.ts
+++ leet-microsoft/longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.ts
@@ -6,5 +6,20 @@
  */
 
 export function lengthOfLongestSubstring(s: string): number {
-  return 0;
+  let longest = 0;
+
+  if (s.length <= 1) return s.length;
+  longest = 1;
+
+  for (let start = 0; start < s.length; start++) {
+    for (let end = start + 1; end < s.length; end++) {
+      if (s.substring(start, end).includes(s[end])) break;
+
+      const length = end - start + 1;
+
+      if (length > longest) longest = length;
+    }
+  }
+
+  return longest;
 }
