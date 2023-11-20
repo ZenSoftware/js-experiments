674f23764d6f378d6bd1e7aa9b6a9d52ef49bf4f
Zen Software <ZenSoftware@users.noreply.github.com>
2023-11-20 11:52:19 -0700

    complexity analysis

--- leet-microsoft/two-sum.ts
+++ leet-microsoft/two-sum.ts
@@ -5,6 +5,10 @@
  * You can return the answer in any order.
  */
 
+/**
+ * Time: O(n^2)
+ * Space: O(n);
+ */
 export function twoSum(nums: number[], target: number): number[] {
   for (let i = 0; i < nums.length - 1; i++) {
     for (let j = i + 1; j < nums.length; j++) {
