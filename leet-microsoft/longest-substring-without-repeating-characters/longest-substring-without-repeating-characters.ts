56d613c469672d884b8f6aa86efd7a4cfbe1ac7b
Zen Software <ZenSoftware@users.noreply.github.com>
2023-11-21 00:26:44 -0700

    efficiency

--- leet-microsoft/longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.ts
+++ leet-microsoft/longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.ts
@@ -6,21 +6,37 @@
  */
 
 export function lengthOfLongestSubstring(s: string): number {
-  let longest = 0;
-
-  if (s.length <= 1) return s.length;
-
-  longest = 1;
-
-  for (let start = 0; start < s.length - 1 && s.length - start + 1 > longest; start++) {
-    for (let end = start + 1; end < s.length; end++) {
-      if (s.substring(start, end).includes(s[end])) break;
-
-      const length = end - start + 1;
-
-      if (length > longest) longest = length;
+  const setMap = new Set();
+  let max = 0;
+  let index = 0;
+  for (let i = 0; i < s.length; i++) {
+    let key = s[i];
+    while (setMap.has(key)) {
+      setMap.delete(s[index]);
+      index++;
     }
+    setMap.add(key);
+    max = Math.max(max, setMap.size);
   }
-
-  return longest;
+  return max;
 }
+
+// export function lengthOfLongestSubstring(s: string): number {
+//   let longest = 0;
+
+//   if (s.length <= 1) return s.length;
+
+//   longest = 1;
+
+//   for (let start = 0; start < s.length - 1 && s.length - start + 1 > longest; start++) {
+//     for (let end = start + 1; end < s.length; end++) {
+//       if (s.substring(start, end).includes(s[end])) break;
+
+//       const length = end - start + 1;
+
+//       if (length > longest) longest = length;
+//     }
+//   }
+
+//   return longest;
+// }
