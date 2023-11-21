fcc16804a93f04260c23013a36ba871b5d045699
Zen Software <ZenSoftware@users.noreply.github.com>
2023-11-20 22:17:38 -0700

    efficiency

--- leet-microsoft/longest-palindromic-substring/longest-palindromic-substring.ts
+++ leet-microsoft/longest-palindromic-substring/longest-palindromic-substring.ts
@@ -14,6 +14,7 @@ export function longestPalindrome(s: string): string {
 
       if (isPalindrome(evaluating) && evaluating.length > longest.length) {
         longest = evaluating;
+        break;
       }
     }
   }
