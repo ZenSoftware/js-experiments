bf120919d0e089d942d07dcf083c627d5a936917
Zen Software <ZenSoftware@users.noreply.github.com>
2023-11-20 21:07:28 -0700

    docs

--- leet-microsoft/longest-palindromic-substring/longest-palindromic-substring.ts
+++ leet-microsoft/longest-palindromic-substring/longest-palindromic-substring.ts
@@ -1,3 +1,8 @@
+/**
+ * https://leetcode.com/problems/longest-palindromic-substring/?envType=featured-list&envId=top-microsoft-questions?envType=featured-list&envId=top-microsoft-questions
+ * Given a string s, return the longest palindromic substring in s.
+ * - 1 <= s.length <= 1000
+ */
 export function longestPalindrome(s: string): string {
   return '';
 }
