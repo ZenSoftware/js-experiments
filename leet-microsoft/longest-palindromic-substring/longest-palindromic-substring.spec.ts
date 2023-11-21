ecb9c9f97393e22722c8eb8a675eb7f044edf9ce
Zen Software <ZenSoftware@users.noreply.github.com>
2023-11-20 22:01:34 -0700

    working solution

--- leet-microsoft/longest-palindromic-substring/longest-palindromic-substring.spec.ts
+++ leet-microsoft/longest-palindromic-substring/longest-palindromic-substring.spec.ts
@@ -1,8 +1,18 @@
-import { longestPalindrome } from './longest-palindromic-substring';
+import { longestPalindrome, isPalindrome } from './longest-palindromic-substring';
 
 describe('Longest Palindromic Substring', () => {
   it('evaluates correctly', () => {
+    expect(longestPalindrome('a')).toEqual('a');
+    expect(longestPalindrome('bb')).toEqual('bb');
     expect(longestPalindrome('babad')).toEqual('bab');
     expect(longestPalindrome('cbbd')).toEqual('bb');
+  });
+
+  it('evaluates if string is a palindrome correctly', () => {
+    expect(isPalindrome('b')).toEqual(true);
+    expect(isPalindrome('bb')).toEqual(true);
+    expect(isPalindrome('bab')).toEqual(true);
+    expect(isPalindrome('zeez')).toEqual(true);
+    expect(isPalindrome('lily')).toEqual(false);
   });
 });
--- leet-microsoft/longest-palindromic-substring/longest-palindromic-substring.ts
+++ leet-microsoft/longest-palindromic-substring/longest-palindromic-substring.ts
@@ -1,8 +1,35 @@
 /**
  * https://leetcode.com/problems/longest-palindromic-substring/?envType=featured-list&envId=top-microsoft-questions?envType=featured-list&envId=top-microsoft-questions
  * Given a string s, return the longest palindromic substring in s.
- * - 1 <= s.length <= 1000
+ * - (1 <= s.length <= 1000)
  */
 export function longestPalindrome(s: string): string {
-  return '';
+  if (s.length <= 1) return s;
+
+  let longest = s[0];
+
+  for (let i = 0; i < s.length - 1; i++) {
+    for (let j = i + 1; j < s.length; j++) {
+      const stringUnderEvaluation = s.substring(i, j + 1);
+
+      if (isPalindrome(stringUnderEvaluation) && stringUnderEvaluation.length > longest.length) {
+        longest = stringUnderEvaluation;
+      }
+    }
+  }
+
+  return longest;
+}
+
+export function isPalindrome(s: string): boolean {
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    if (s[left] !== s[right]) return false;
+
+    left++;
+    right--;
+  }
+  return true;
 }
