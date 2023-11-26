e82635674109dac3e5c1bfc69d51ab1bd77bd69a
Zen Software <ZenSoftware@users.noreply.github.com>
2023-11-25 18:52:22 -0700

    Container With Most Water skeleton

---
+++ leet-microsoft/container-with-most-water/container-with-most-water.spec.ts
@@ -0,0 +1,7 @@
+import { maxArea } from './container-with-most-water';
+
+describe('Container With Most Water', () => {
+  it('evaluates correctly', () => {
+    expect(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7])).toEqual(49);
+  });
+});
---
+++ leet-microsoft/container-with-most-water/container-with-most-water.ts
@@ -0,0 +1,13 @@
+/**
+ * https://leetcode.com/problems/container-with-most-water/?envType=featured-list&envId=top-microsoft-questions%3FenvType%3Dfeatured-list&envId=top-microsoft-questions
+ * You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+ * Find two lines that together with the x-axis form a container, such that the container contains the most water.
+ * Return the maximum amount of water a container can store.
+ * Notice that you may not slant the container.
+ * n == height.length
+ * 2 <= n <= 105
+ * 0 <= height[i] <= 104
+ */
+export function maxArea(height: number[]): number {
+  return 0;
+}
