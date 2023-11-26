b0697fca5b85956b88bdcd96b8b20392b0194c46
Zen Software <ZenSoftware@users.noreply.github.com>
2023-11-26 06:13:05 -0700

    two-pointer approach

--- leet-microsoft/three-sum-closest/three-sum-closest.ts
+++ leet-microsoft/three-sum-closest/three-sum-closest.ts
@@ -7,8 +7,39 @@
  * -1000 <= nums[i] <= 1000
  * -104 <= target <= 104
  */
+
+/**
+ * Two-pointer approach
+ * Time: O(n^2)
+ * Space: O(1)
+ */
 export function threeSumClosest(nums: number[], target: number): number {
+  nums.sort((a, b) => a - b);
+  let bestDistance = Infinity;
   let best = nums[0] + nums[1] + nums[2];
+
+  for (let fp = 0; fp < nums.length - 2; fp++) {
+    let lp = fp + 1;
+    let rp = nums.length - 1;
+
+    while (lp < rp) {
+      const sum = nums[fp] + nums[lp] + nums[rp];
+      const currentDistance = Math.abs(target - sum);
+
+      if (currentDistance === 0) {
+        return target;
+      } else if (currentDistance < bestDistance) {
+        best = sum;
+        bestDistance = currentDistance;
+      }
+
+      if (sum < target) {
+        lp++;
+      } else {
+        rp--;
+      }
+    }
+  }
 
   return best;
 }
