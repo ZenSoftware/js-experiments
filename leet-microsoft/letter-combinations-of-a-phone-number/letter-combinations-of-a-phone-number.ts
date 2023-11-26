3f07f4cb648dad68c3c56045cdded0dd7931287e
Zen Software <ZenSoftware@users.noreply.github.com>
2023-11-26 06:34:27 -0700

    phone digit to letters mapping

--- leet-microsoft/letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.ts
+++ leet-microsoft/letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.ts
@@ -8,3 +8,14 @@
 export function letterCombinations(digits: string): string[] {
   return [];
 }
+
+const PHONE_DIGIT_LETTERS = {
+  2: ['a', 'b', 'c'],
+  3: ['d', 'e', 'f'],
+  4: ['g', 'h', 'i'],
+  5: ['j', 'k', 'l'],
+  6: ['m', 'n', 'o'],
+  7: ['p', 'q', 'r', 's'],
+  8: ['t', 'u', 'v'],
+  9: ['w', 'x', 'y', 'z'],
+};
