5cda5ee9d005d72a4f26f7d973d175239c36d98d
Zen Software <ZenSoftware@users.noreply.github.com>
2023-11-26 14:35:17 -0700

    cvb

--- leet-microsoft/remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.ts
+++ leet-microsoft/remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.ts
@@ -19,3 +19,31 @@ export class ListNode {
 export function removeNthFromEnd(head: ListNode | null, n: number): ListNode | null {
   return null;
 }
+
+function toList(vals: number[]) {
+  let pointer = new ListNode(vals[vals.length - 1]);
+
+  for (let i = vals.length - 2; i >= 0; i--) {
+    const node = new ListNode(vals[i]);
+    node.next = pointer;
+    pointer = node;
+  }
+
+  return pointer;
+}
+
+function printList(head: ListNode | null) {
+  if (head === null) return [];
+
+  const result: number[] = [];
+  let pointer: any = head;
+
+  do {
+    result.push(pointer.val);
+    pointer = pointer.next;
+  } while (pointer !== null);
+
+  return result;
+}
+
+console.log(printList(toList([1, 2, 3, 4, 5])));
