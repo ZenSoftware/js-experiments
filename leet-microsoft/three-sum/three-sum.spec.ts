997f234d2afbf4ed87bba5ff5c28adfe063f1216
Zen Software <ZenSoftware@users.noreply.github.com>
2023-11-25 20:43:33 -0700

    skeleton

---
+++ leet-microsoft/three-sum/three-sum.spec.ts
@@ -0,0 +1,12 @@
+import { threeSum } from './three-sum';
+
+describe('3Sum', () => {
+  it('evaluates correctly', () => {
+    expect(threeSum([-1, 0, 1, 2, -1, -4])).toEqual([
+      [-1, -1, 2],
+      [-1, 0, 1],
+    ]);
+    expect(threeSum([0, 1, 1])).toEqual([]);
+    expect(threeSum([0, 0, 0])).toEqual([[0, 0, 0]]);
+  });
+});
---
+++ leet-microsoft/three-sum/three-sum.ts
@@ -0,0 +1,10 @@
+/**
+ * https://leetcode.com/problems/3sum/
+ * Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+ * Notice that the solution set must not contain duplicate triplets.
+ * 3 <= nums.length <= 3000
+ * -105 <= nums[i] <= 105
+ */
+export function threeSum(nums: number[]): number[][] {
+  return [];
+}
