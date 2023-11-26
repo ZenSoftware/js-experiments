b6b28a22e9840276bdce61f0284201ab7de74c33
Zen Software <ZenSoftware@users.noreply.github.com>
2023-11-26 13:37:01 -0700

    solution

--- leet-microsoft/four-sum/four-sum.spec.ts
+++ leet-microsoft/four-sum/four-sum.spec.ts
@@ -2,11 +2,12 @@ import { fourSum } from './four-sum';
 
 describe('4Sum', () => {
   it('evaluates correctly', () => {
-    // expect(fourSum([1, 0, -1, 0, -2, 2], 0)).toEqual([
-    //   [-2, -1, 1, 2],
-    //   [-2, 0, 0, 2],
-    //   [-1, 0, 0, 1],
-    // ]);
+    expect(fourSum([1, 0, -1, 0, -2, 2], 0)).toEqual([
+      [-2, -1, 1, 2],
+      [-2, 0, 0, 2],
+      [-1, 0, 0, 1],
+    ]);
     expect(fourSum([2, 2, 2, 2], 8)).toEqual([[2, 2, 2, 2]]);
+    expect(fourSum([2, 2, 2, 2, 2], 8)).toEqual([[2, 2, 2, 2]]);
   });
 });
--- leet-microsoft/four-sum/four-sum.ts
+++ leet-microsoft/four-sum/four-sum.ts
@@ -10,15 +10,44 @@
  */
 export function fourSum(nums: number[], target: number): number[][] {
   const results: number[][] = [];
-  let lp1 = 0;
-  let lp2 = 1;
-  let rp2 = nums.length - 2;
-  let rp1 = nums.length - 1;
+  nums.sort((a, b) => a - b);
 
-  const sum = nums[lp1] + nums[lp2] + nums[rp2] + nums[rp1];
-  const quaduplet = [nums[lp1], nums[lp2], nums[rp2], nums[rp1]];
-  if (sum === target) {
-    results.push(quaduplet);
+  for (let i = 0; i < nums.length - 3; i++) {
+    for (let j = i + 1; j < nums.length - 2; j++) {
+      const f = nums[i];
+      const s = nums[j];
+      let left = j + 1;
+      let right = nums.length - 1;
+
+      while (left < right) {
+        const sum = f + s + nums[left] + nums[right];
+        if (sum === target) {
+          results.push([f, s, nums[left], nums[right]]);
+
+          while (nums[left] === nums[left + 1]) {
+            left++;
+          }
+
+          while (nums[right] === nums[right - 1]) {
+            right++;
+          }
+
+          left++;
+          right--;
+        } else if (sum < target) {
+          left++;
+        } else {
+          right--;
+        }
+      }
+
+      while (nums[j] === nums[j + 1]) {
+        j++;
+      }
+      while (nums[i] === nums[i + 1]) {
+        i++;
+      }
+    }
   }
 
   return results;
