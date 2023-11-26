96047e39257ec4f6e33f9d0752a694028569895f
Zen Software <ZenSoftware@users.noreply.github.com>
2023-11-25 21:01:18 -0700

    partial solution

--- leet-microsoft/three-sum/three-sum.ts
+++ leet-microsoft/three-sum/three-sum.ts
@@ -6,5 +6,19 @@
  * -105 <= nums[i] <= 105
  */
 export function threeSum(nums: number[]): number[][] {
-  return [];
+  const result: number[][] = [];
+
+  for (let i = 0; i < nums.length - 2; i++) {
+    for (let j = i + 1; j < nums.length - 1; j++) {
+      for (let k = j + 1; k < nums.length; k++) {
+        if (nums[i] + nums[j] + nums[k] === 0) {
+          result.push([nums[i], nums[j], nums[k]]);
+        }
+      }
+    }
+  }
+
+  return result;
 }
+
+console.log(threeSum([-1, 0, 1, 2, -1, -4]));
