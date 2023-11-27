ea10181c8269f36f6ccb30d1144b252331023eb1
Zen Software <ZenSoftware@users.noreply.github.com>
2023-11-26 18:31:03 -0700

    added test

--- leet-microsoft/valid-parentheses/valid-parentheses.spec.ts
+++ leet-microsoft/valid-parentheses/valid-parentheses.spec.ts
@@ -3,5 +3,7 @@ import { isValid } from './valid-parentheses';
 describe('Valid Parentheses', () => {
   it('evaluates correctly', () => {
     expect(isValid('()')).toEqual(true);
+    expect(isValid('()[]{}')).toEqual(true);
+    expect(isValid('(]')).toEqual(false);
   });
 });
