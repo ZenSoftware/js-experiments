fc017ef463f9dc3ab7b0e4eabbde9b1cf34d9c2e
Zen Software <ZenSoftware@users.noreply.github.com>
2023-11-27 04:23:50 -0700

    solutions

--- leet-microsoft/generate-parentheses/generate-parentheses.ts
+++ leet-microsoft/generate-parentheses/generate-parentheses.ts
@@ -4,5 +4,18 @@
  * 1 <= n <= 8
  */
 export function generateParenthesis(n: number): string[] {
-  return [];
+  const ans = [];
+  backtrack(ans, '', 0, 0, n);
+  return ans;
+}
+
+function backtrack(ans: string[], cur: string, open: number, close: number, max: number) {
+  if (close === max) {
+    ans.push(cur);
+    return;
+  }
+
+  if (open < max) backtrack(ans, cur + '(', open + 1, close, max);
+
+  if (close < open) backtrack(ans, cur + ')', open, close + 1, max);
 }
