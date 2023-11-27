7c40fde1b7ee70816296c5f8fe07c590b4803998
Zen Software <ZenSoftware@users.noreply.github.com>
2023-11-26 18:26:00 -0700

    solution

--- leet-microsoft/number-of-islands/number-of-islands.spec.ts
+++ leet-microsoft/number-of-islands/number-of-islands.spec.ts
@@ -2,12 +2,20 @@ import { numIslands } from './number-of-islands';
 
 describe('Number of Islands', () => {
   it('evaluates correctly', () => {
-    const grid = [
-      ['1', '1', '1', '1', '0'],
-      ['1', '1', '0', '1', '0'],
+    // const grid = [
+    //   ['1', '1', '1', '1', '0'],
+    //   ['1', '1', '0', '1', '0'],
+    //   ['1', '1', '0', '0', '0'],
+    //   ['0', '0', '0', '0', '0'],
+    // ];
+    // expect(numIslands(grid)).toEqual(1);
+
+    const grid2 = [
       ['1', '1', '0', '0', '0'],
-      ['0', '0', '0', '0', '0'],
+      ['1', '1', '0', '0', '0'],
+      ['0', '0', '1', '0', '0'],
+      ['0', '0', '0', '1', '1'],
     ];
-    expect(numIslands(grid)).toEqual(1);
+    expect(numIslands(grid2)).toEqual(3);
   });
 });
--- leet-microsoft/number-of-islands/number-of-islands.ts
+++ leet-microsoft/number-of-islands/number-of-islands.ts
@@ -9,5 +9,33 @@
  */
 
 export function numIslands(grid: string[][]): number {
-  return 0;
+  let count = 0;
+  for (let i = 0; i < grid.length; i++) {
+    const row = grid[i];
+    for (let j = 0; j < row.length; j++) {
+      if (grid[i][j] === '1') {
+        ++count;
+        dfs(grid, i, j);
+      }
+    }
+  }
+  return count;
+}
+
+function dfs(grid: string[][], row, col) {
+  if (
+    row < 0 ||
+    row > grid.length - 1 ||
+    col < 0 ||
+    col > grid[row].length - 1 ||
+    grid[row][col] === '0'
+  ) {
+    return;
+  }
+
+  grid[row][col] = '0';
+  dfs(grid, row + 1, col);
+  dfs(grid, row - 1, col);
+  dfs(grid, row, col + 1);
+  dfs(grid, row, col - 1);
 }
