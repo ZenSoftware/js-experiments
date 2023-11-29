9b3322755d10a402215b1f7df3248912822001bb
Zen Software <ZenSoftware@users.noreply.github.com>
2023-11-29 16:40:15 -0700

    solution

--- leet-microsoft/next-permutation/next-permutation.spec.ts
+++ leet-microsoft/next-permutation/next-permutation.spec.ts
@@ -2,10 +2,13 @@ import { nextPermutation } from './next-permutation';
 
 describe('Next Permutation', () => {
   it('evaluates correcty', () => {
-    const nums1 = [1, 2, 3];
-    expect(nextPermutation(nums1)).toEqual([1, 3, 2]);
+    const nums1 = [1, 2, 4, 3];
+    expect(nextPermutation(nums1)).toEqual([1, 3, 2, 4]);
 
-    const nums2 = [3, 2, 1];
-    expect(nextPermutation(nums2)).toEqual([1, 3, 2]);
+    // const nums1 = [1, 2, 3];
+    // expect(nextPermutation(nums1)).toEqual([1, 3, 2]);
+
+    // const nums2 = [3, 2, 1];
+    // expect(nextPermutation(nums2)).toEqual([1, 3, 2]);
   });
 });
--- leet-microsoft/next-permutation/next-permutation.ts
+++ leet-microsoft/next-permutation/next-permutation.ts
@@ -11,47 +11,57 @@
  * 1 <= nums.length <= 100
  * 0 <= nums[i] <= 100
  */
-export function nextPermutation(nums: number[]): void {
-  swapAt(nums, 0, 1);
+
+/**
+ * Solution: https://leetcode.com/problems/next-permutation/solutions/2238691/functional-declarative-javascript-typescript-solution/?envType=featured-list&envId=top-microsoft-questions%3FenvType%3Dfeatured-list&envId=top-microsoft-questions
+ */
+
+export function nextPermutation(nums: number[]) {
+  const firstIndex = lastIndexWhere(nums, (num, i) => num < nums[i + 1]);
+
+  if (firstIndex === undefined) {
+    reverse(nums, 0, nums.length - 1);
+    return nums;
+  }
+
+  const secondIndex = lastIndexWhere(nums, (num, i) => num > nums[firstIndex]) as number;
+
+  swap(nums, firstIndex, secondIndex);
+
+  reverse(nums, firstIndex + 1, nums.length - 1);
+  return nums;
 }
 
-function swapAt(nums: number[], start: number, end: number) {
+function swap(nums: number[], start: number, end: number) {
   const temp = nums[start];
   nums[start] = nums[end];
   nums[end] = temp;
   return nums;
 }
 
-function lexMax(nums: number[]) {
-  return lexSort(nums, 'desc')[0];
-}
-
-function lexSort(nums: number[], order: 'asc' | 'desc' = 'asc') {
-  if (order === 'desc') {
-    return nums.sort((a, b) => {
-      const as = a.toString();
-      const bs = b.toString();
-      if (as === bs) return 0;
-      return as < bs ? 1 : -1;
-    });
-  } else {
-    return nums.sort((a, b) => {
-      const as = a.toString();
-      const bs = b.toString();
-      if (as === bs) return 0;
-      return as > bs ? 1 : -1;
-    });
+function lastIndexWhere(nums: number[], condition: (num: number, i: number) => boolean) {
+  for (let i = nums.length - 1; i >= 0; i--) {
+    if (condition(nums[i], i)) return i;
   }
+  return undefined;
 }
 
-/**
- * [1,2,3]
- * [1,3,2]
- * [2,1,3]
- * [2,3,1]
- * [3,1,2]
- * [3,2,1]
- */
-console.log(swapAt([1, 2, 3], 0, 1));
-console.log(lexSort([1, 2, 10]));
-console.log(lexMax([1, 2, 10]));
+function reverse(nums: number[], start: number, end: number) {
+  while (start < end) {
+    swap(nums, start, end);
+    start++;
+    end--;
+  }
+  return nums;
+}
+
+const step1 = nextPermutation([1, 2, 3, 4]);
+console.log(step1);
+const step2 = nextPermutation(step1);
+console.log(step2);
+const step3 = nextPermutation(step2);
+console.log(step3);
+const step4 = nextPermutation(step3);
+console.log(step4);
+const step5 = nextPermutation(step4);
+console.log(step5);
