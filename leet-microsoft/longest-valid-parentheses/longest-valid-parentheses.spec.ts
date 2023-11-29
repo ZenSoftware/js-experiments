c2deaa09dfa8b052b2c3c6a1ae60a1b6d6707a3a
Zen Software <ZenSoftware@users.noreply.github.com>
2023-11-29 16:44:32 -0700

    tests

--- leet-microsoft/longest-valid-parentheses/longest-valid-parentheses.spec.ts
+++ leet-microsoft/longest-valid-parentheses/longest-valid-parentheses.spec.ts
@@ -2,6 +2,8 @@ import { longestValidParentheses } from './longest-valid-parentheses';
 
 describe('Longest Valid Parentheses', () => {
   it('evaluates correcty', () => {
-    // expect().toEqual();
+    expect(longestValidParentheses('(()')).toEqual(2);
+    expect(longestValidParentheses(')()())')).toEqual(4);
+    expect(longestValidParentheses('')).toEqual(0);
   });
 });
