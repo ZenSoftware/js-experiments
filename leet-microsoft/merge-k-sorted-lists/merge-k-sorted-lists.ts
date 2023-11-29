c0777c39824d9fc4be074a3b1d91b8b4306e1775
Zen Software <ZenSoftware@users.noreply.github.com>
2023-11-28 17:03:41 -0700

    divide and conquer solution

--- leet-microsoft/merge-k-sorted-lists/merge-k-sorted-lists.ts
+++ leet-microsoft/merge-k-sorted-lists/merge-k-sorted-lists.ts
@@ -19,7 +19,37 @@ class ListNode {
 }
 
 export function mergeKLists(lists: Array<ListNode | null>): ListNode | null {
-  return null;
+  if (lists === null || lists.length === 0) {
+    return null;
+  }
+
+  return divide(lists, 0, lists.length - 1);
+}
+
+function divide(lists: Array<ListNode | null>, start: number, end: number): ListNode | null {
+  if (start === end) return lists[start];
+
+  const mid = Math.floor((start + end) / 2);
+  const left = divide(lists, start, mid);
+  const right = divide(lists, mid + 1, end);
+
+  return conquer(left, right);
+}
+
+function conquer(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+  if (l1 === null) return l2;
+  if (l2 === null) return l1;
+
+  let result: ListNode | null;
+  if (l1.val < l2.val) {
+    result = l1;
+    result.next = conquer(l1.next, l2);
+  } else {
+    result = l2;
+    result.next = conquer(l1, l2.next);
+  }
+
+  return result;
 }
 
 export function arrayToList(nums: number[]) {
