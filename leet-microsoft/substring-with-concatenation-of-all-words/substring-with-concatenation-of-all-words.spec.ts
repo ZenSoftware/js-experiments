fafae41a56aa5c534d123f7fb967861a83920f44
Zen Software <ZenSoftware@users.noreply.github.com>
2023-11-29 04:48:52 -0700

    skeleton

---
+++ leet-microsoft/substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.spec.ts
@@ -0,0 +1,9 @@
+import { findSubstring } from './substring-with-concatenation-of-all-words';
+
+describe('Reverse Integer', () => {
+  it('evaluates correctly', () => {
+    const words = ['abcdef', 'abefcd', 'cdabef', 'cdefab', 'efabcd', 'efcdab'];
+    findSubstring('', words);
+    // expect().toEqual();
+  });
+});
---
+++ leet-microsoft/substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.ts
@@ -0,0 +1,14 @@
+/**
+ * https://leetcode.com/problems/substring-with-concatenation-of-all-words/?envType=featured-list&envId=top-microsoft-questions%3FenvType%3Dfeatured-list&envId=top-microsoft-questions
+ * You are given a string s and an array of strings words. All the strings of words are of the same length.
+ * A concatenated substring in s is a substring that contains all the strings of any permutation of words concatenated.
+ * For example, if words = ["ab","cd","ef"], then "abcdef", "abefcd", "cdabef", "cdefab", "efabcd", and "efcdab" are all concatenated strings. "acdbef" is not a concatenated substring because it is not the concatenation of any permutation of words.
+ * Return the starting indices of all the concatenated substrings in s. You can return the answer in any order.
+ * 1 <= s.length <= 104
+ * 1 <= words.length <= 5000
+ * 1 <= words[i].length <= 30
+ * s and words[i] consist of lowercase English letters.
+ */
+export function findSubstring(s: string, words: string[]): number[] {
+  return [];
+}
