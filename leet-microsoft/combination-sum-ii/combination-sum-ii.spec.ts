b666836df2aa3cae41bb39d3bf99a9bf6b90cff5
Zen Software <ZenSoftware@users.noreply.github.com>
2023-11-30 01:20:35 -0700

    skeleton

---
+++ leet-microsoft/combination-sum-ii/combination-sum-ii.spec.ts
@@ -0,0 +1,14 @@
+import { combinationSum2 } from './combination-sum-ii';
+
+describe('Combination Sum II', () => {
+  it('evaluates correctly', () => {
+    expect(combinationSum2([10, 1, 2, 7, 6, 1, 5], 8)).toEqual([
+      [1, 1, 6],
+      [1, 2, 5],
+      [1, 7],
+      [2, 6],
+    ]);
+
+    expect(combinationSum2([2, 5, 2, 1, 2], 5)).toEqual([[1, 2, 2], [5]]);
+  });
+});
---
+++ leet-microsoft/combination-sum-ii/combination-sum-ii.ts
@@ -0,0 +1,12 @@
+/**
+ * https://leetcode.com/problems/combination-sum-ii/
+ * Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate numbers sum to target.
+ * Each number in candidates may only be used once in the combination.
+ * Note: The solution set must not contain duplicate combinations.
+ * 1 <= candidates.length <= 100
+ * 1 <= candidates[i] <= 50
+ * 1 <= target <= 30
+ */
+export function combinationSum2(candidates: number[], target: number): number[][] {
+  return [];
+}
