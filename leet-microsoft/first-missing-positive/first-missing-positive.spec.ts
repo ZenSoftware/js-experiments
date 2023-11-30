ce82f5bb51191e10c418a83d4765547889af9813
Zen Software <ZenSoftware@users.noreply.github.com>
2023-11-30 15:33:38 -0700

    solution

--- leet-microsoft/first-missing-positive/first-missing-positive.spec.ts
+++ leet-microsoft/first-missing-positive/first-missing-positive.spec.ts
@@ -2,8 +2,8 @@ import { firstMissingPositive } from './first-missing-positive';
 
 describe('First Missing Positive', () => {
   it('evaluates correctly', () => {
-    expect(firstMissingPositive([1, 2, 0])).toEqual(3);
     expect(firstMissingPositive([3, 4, -1, 1])).toEqual(2);
     expect(firstMissingPositive([7, 8, 9, 11, 12])).toEqual(1);
+    expect(firstMissingPositive([1, 2, 0])).toEqual(3);
   });
 });
--- leet-microsoft/first-missing-positive/first-missing-positive.ts
+++ leet-microsoft/first-missing-positive/first-missing-positive.ts
@@ -6,5 +6,21 @@
  * -231 <= nums[i] <= 231 - 1
  */
 export function firstMissingPositive(nums: number[]): number {
-  return 1;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] < 0) nums[i] = 0;
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    const idx = Math.abs(nums[i]) - 1;
+    if (0 <= idx && idx < nums.length) {
+      if (nums[idx] === 0) nums[idx] = -(nums.length + 1);
+      else if (nums[idx] > 0) nums[idx] *= -1;
+    }
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] >= 0) return i + 1;
+  }
+
+  return nums.length + 1;
 }
