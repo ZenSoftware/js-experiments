076e94d1c3ce6b75fa7c2d518db01727101b9e00
Zen Software <ZenSoftware@users.noreply.github.com>
2023-11-29 17:00:48 -0700

    solution

--- leet-microsoft/longest-valid-parentheses/longest-valid-parentheses.spec.ts
+++ leet-microsoft/longest-valid-parentheses/longest-valid-parentheses.spec.ts
@@ -1,9 +1,13 @@
-import { longestValidParentheses } from './longest-valid-parentheses';
+import { isValid, longestValidParentheses } from './longest-valid-parentheses';
 
 describe('Longest Valid Parentheses', () => {
   it('evaluates correcty', () => {
     expect(longestValidParentheses('(()')).toEqual(2);
     expect(longestValidParentheses(')()())')).toEqual(4);
     expect(longestValidParentheses('')).toEqual(0);
+  });
+
+  it('validates correctly', () => {
+    expect(isValid('()')).toEqual(true);
   });
 });
--- leet-microsoft/longest-valid-parentheses/longest-valid-parentheses.ts
+++ leet-microsoft/longest-valid-parentheses/longest-valid-parentheses.ts
@@ -5,5 +5,26 @@
  * s[i] is '(', or ')'.
  */
 export function longestValidParentheses(s: string): number {
-  return 0;
+  let longest = 0;
+  for (let i = 0; i < s.length - 1; i++) {
+    for (let j = i + 1; j < s.length; j++) {
+      const str = s.substring(i, j + 1);
+      if (isValid(str) && str.length > longest) longest = str.length;
+    }
+  }
+  return longest;
 }
+
+export function isValid(s: string) {
+  const memo: string[] = [];
+  for (let i = 0; i < s.length; i++) {
+    if (s.charAt(i) === '(') memo.push('(');
+    else {
+      const current = memo.pop();
+      if (current !== '(') return false;
+    }
+  }
+  return memo.length === 0;
+}
+
+console.log(isValid('((()))'));
