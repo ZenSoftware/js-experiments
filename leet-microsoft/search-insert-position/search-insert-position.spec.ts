046761896374caf1e45dc5f15837c3e8832d11f9
Zen Software <ZenSoftware@users.noreply.github.com>
2023-11-29 23:39:35 -0700

    solution

--- leet-microsoft/search-insert-position/search-insert-position.spec.ts
+++ leet-microsoft/search-insert-position/search-insert-position.spec.ts
@@ -2,6 +2,10 @@ import { searchInsert } from './search-insert-position';
 
 describe('Search Insert Position', () => {
   it('evaluates correctly', () => {
-    // expect().toEqual();
+    expect(searchInsert([1, 3, 5, 6], 5)).toEqual(2);
+    expect(searchInsert([1, 3, 5, 6], 2)).toEqual(1);
+    expect(searchInsert([1, 3, 5, 6], 7)).toEqual(4);
+    expect(searchInsert([1, 3], 0)).toEqual(0);
+    expect(searchInsert([1, 3], 2)).toEqual(1);
   });
 });
--- leet-microsoft/search-insert-position/search-insert-position.ts
+++ leet-microsoft/search-insert-position/search-insert-position.ts
@@ -8,5 +8,13 @@
  * -104 <= target <= 104
  */
 export function searchInsert(nums: number[], target: number): number {
-  return -1;
+  let start = 0;
+  let end = nums.length - 1;
+  while (start <= end) {
+    const mid = Math.floor((start + end) / 2);
+    if (nums[mid] === target) return mid;
+    else if (target < nums[mid]) end = mid - 1;
+    else start = mid + 1;
+  }
+  return start;
 }
