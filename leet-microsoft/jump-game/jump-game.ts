130b827142014d742ae07b0224786896149546f1
Zen Software <ZenSoftware@users.noreply.github.com>
2023-12-01 00:27:40 -0700

    refactor

--- leet-microsoft/jump-game/jump-game.ts
+++ leet-microsoft/jump-game/jump-game.ts
@@ -12,10 +12,9 @@ export function canJump(nums: number[]): boolean {
   let left = nums[0];
 
   for (let i = 1; i < nums.length; i++) {
-    if (left === 0) {
-      return false;
-    }
+    if (left === 0) return false;
     left = Math.max(left - 1, nums[i]);
   }
+
   return true;
 }
