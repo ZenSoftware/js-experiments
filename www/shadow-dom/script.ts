26bd22bf43b74ec884d074074915fcb1d48a5af3
Zen Software <ZenSoftware@users.noreply.github.com>
2023-12-01 12:47:17 -0700

    style shadow

--- www/shadow-dom/script.ts
+++ www/shadow-dom/script.ts
@@ -4,10 +4,16 @@ const sampleSpan = document.createElement('span');
 sampleSpan.innerHTML = 'sample span';
 shadow.appendChild(sampleSpan);
 
+const styleShadowButton = document.querySelector('#style-shadow') as HTMLButtonElement;
+styleShadowButton.addEventListener('click', () => {
+  const styleTemplate = document.querySelector('#style-template') as HTMLTemplateElement;
+  shadow.appendChild(styleTemplate.content);
+});
+
 const uppercaseButton = document.querySelector('#upper') as HTMLButtonElement;
 uppercaseButton.addEventListener('click', () => {
-  const allSpans = document.querySelectorAll('span');
-  allSpans.forEach(s => {
+  const allDocumentSpans = document.querySelectorAll('span');
+  allDocumentSpans.forEach(s => {
     s.innerText = s.innerText.toLocaleUpperCase();
   });
 });
