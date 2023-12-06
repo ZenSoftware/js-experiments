f377f304f74c073975a59e174f78f033df235863
Zen Software <ZenSoftware@users.noreply.github.com>
2023-12-05 22:18:16 -0700

    solution

---
+++ experiments/convert-curry.ts
@@ -0,0 +1,15 @@
+function curry(fn: Function) {
+  return function curried(...args) {
+    if (args.length >= fn.length) {
+      return fn(...args);
+    } else {
+      return function (...next) {
+        return curried(...args, ...next);
+      };
+    }
+  };
+}
+
+const sum = (x, y, z) => x + y + z;
+const curriedSum = curry(sum);
+console.log(curriedSum(1, 2)(3));
