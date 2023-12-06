ee713a3faccd6bbdefc8e4bd58ee34c4a0e5b8f3
Zen Software <ZenSoftware@users.noreply.github.com>
2023-12-05 19:40:40 -0700

    shared worker

--- www/webworker2/script.ts
+++ www/webworker2/script.ts
@@ -1,9 +1,23 @@
-const myWorker = new Worker('/webworker2/worker.js');
+if (!!window.Worker) {
+  // const myWorker = new Worker('/webworker2/worker.js');
 
-document.getElementById('sample-post')!.addEventListener('click', () => {
-  myWorker.postMessage({ lily: `Can you hear me?` });
-});
+  // document.getElementById('sample-post')!.addEventListener('click', () => {
+  //   myWorker.postMessage({ lily: `Can you hear me?` });
+  // });
 
-myWorker.onmessage = ev => {
-  console.log('recieved message from worker', ev.data);
-};
+  // myWorker.onmessage = ev => {
+  //   console.log('recieved message from worker', ev.data);
+  // };
+
+  // ===================================================================
+
+  const mySharedWorker = new SharedWorker('/webworker2/shared-worker.js');
+
+  document.getElementById('sample-shared-post')!.addEventListener('click', () => {
+    mySharedWorker.port.postMessage({ zen: 'wake up' });
+  });
+
+  mySharedWorker.port.onmessage = ev => {
+    console.log('main script recieved shared worker data:', ev.data);
+  };
+}
