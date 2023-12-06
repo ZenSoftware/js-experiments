de05961b75d685ca1061cfdfa0e827793eb7655a
Zen Software <ZenSoftware@users.noreply.github.com>
2023-12-06 06:11:37 -0700

    greedy but not all cases

---
+++ experiments/coin-problem.ts
@@ -0,0 +1,28 @@
+function change(coins: number[], target: number) {
+  coins.sort((a, b) => b - a);
+  let remaining = target;
+  const result = new Map<number, number>();
+
+  for (let i = 0; i < coins.length; i++) {
+    const coin = coins[i];
+    const numCoins = Math.floor(remaining / coin);
+    if (numCoins > 0) {
+      remaining -= numCoins * coin;
+      result.set(coin, numCoins);
+    }
+  }
+
+  let coinCount = 0;
+  for (const numCoins of result.values()) {
+    coinCount += numCoins;
+  }
+
+  return coinCount;
+}
+
+console.log(change([1, 5, 10, 25], 234));
+// 9x25c = 225
+// 1x5c = 230
+// 4x1c = 234
+
+// 14 coins
