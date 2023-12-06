47b02283642d9d80809bfb38ad4ea85db62a5286
Zen Software <ZenSoftware@users.noreply.github.com>
2023-12-05 17:58:25 -0700

    lily?

--- www/webworker2/script.ts
+++ www/webworker2/script.ts
@@ -1,3 +1,9 @@
-if (window.Worker) {
-  const myWorker = new Worker('/webworker2/worker.js');
-}
+const myWorker = new Worker('/webworker2/worker.js');
+
+document.getElementById('sample-post')!.addEventListener('click', () => {
+  myWorker.postMessage({ lily: `Can you hear me?` });
+});
+
+myWorker.onmessage = ev => {
+  console.log('recieved message from worker', ev.data);
+};
--- www/webworker2/worker.js
+++ www/webworker2/worker.js
@@ -1,1 +1,7 @@
-console.log('from worker');
+self.addEventListener('message', ev => {
+  console.log('in worker recieved: ', ev.data);
+});
+
+setInterval(() => {
+  postMessage({ lily: 'Do you know who I am?' });
+}, 3400);
