ae11447e033385046de5a44f16371cab84243477
Zen Software <ZenSoftware@users.noreply.github.com>
2023-12-07 00:48:47 -0700

    solution

--- leet-microsoft/spiral-matrix/spiral-matrix.ts
+++ leet-microsoft/spiral-matrix/spiral-matrix.ts
@@ -7,5 +7,43 @@
  * -100 <= matrix[i][j] <= 100
  */
 export function spiralOrder(matrix: number[][]): number[] {
-  return [];
+  const result: number[] = [];
+  const numRows = matrix.length;
+  const numCols = matrix[0].length;
+  const size = numRows * numCols;
+
+  let top = 0;
+  let bottom = numRows - 1;
+  let left = 0;
+  let right = numCols - 1;
+
+  function isDone() {
+    return result.length === size;
+  }
+
+  while (true) {
+    for (let col = left; col <= right; col++) {
+      result.push(matrix[top][col]);
+      if (isDone()) return result;
+    }
+    top++;
+
+    for (let row = top; row <= bottom; row++) {
+      result.push(matrix[row][right]);
+      if (isDone()) return result;
+    }
+    right--;
+
+    for (let col = right; col >= left; col--) {
+      result.push(matrix[bottom][col]);
+      if (isDone()) return result;
+    }
+    bottom--;
+
+    for (let row = bottom; row >= top; row--) {
+      result.push(matrix[row][left]);
+      if (isDone()) return result;
+    }
+    left++;
+  }
 }
