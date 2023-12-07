e7ac688751a64fab7af4c7a91f69ed0e5491c76b
Zen Software <ZenSoftware@users.noreply.github.com>
2023-12-06 17:57:45 -0700

    perms

--- leet-microsoft/permutations/permutations.ts
+++ leet-microsoft/permutations/permutations.ts
@@ -6,5 +6,21 @@
  * All the integers of nums are unique.
  */
 export function permute(nums: number[]): number[][] {
-  return [];
+  if (nums.length === 0) return [[]];
+
+  const firstEl = nums[0];
+  const rest = nums.slice(1);
+  const permsWithoutFirst = permute(rest);
+  const result: number[][] = [];
+
+  for (let permWithoutFirst of permsWithoutFirst) {
+    for (let i = 0; i <= permWithoutFirst.length; i++) {
+      const withFirst = [...permWithoutFirst.slice(0, i), firstEl, ...permWithoutFirst.slice(i)];
+      result.push(withFirst);
+    }
+  }
+
+  return result;
 }
+
+console.log(permute([1, 2, 3]));
