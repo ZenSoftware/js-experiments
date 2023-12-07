bffa1f41b79fafb2ad4398b957af1d85055f5843
Zen Software <ZenSoftware@users.noreply.github.com>
2023-12-07 00:16:52 -0700

    skeleton

---
+++ leet-microsoft/spiral-matrix/spiral-matrix.ts
@@ -0,0 +1,11 @@
+/**
+ * https://leetcode.com/problems/spiral-matrix/
+ * Given an m x n matrix, return all elements of the matrix in spiral order.
+ * m == matrix.length
+ * n == matrix[i].length
+ * 1 <= m, n <= 10
+ * -100 <= matrix[i][j] <= 100
+ */
+export function spiralOrder(matrix: number[][]): number[] {
+  return [];
+}
