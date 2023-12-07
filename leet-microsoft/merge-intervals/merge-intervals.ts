38bd3aa515dbdabfe34e87aed6d5c5180c357b5f
Zen Software <ZenSoftware@users.noreply.github.com>
2023-12-07 04:19:16 -0700

    solution

--- leet-microsoft/merge-intervals/merge-intervals.ts
+++ leet-microsoft/merge-intervals/merge-intervals.ts
@@ -7,39 +7,22 @@
  */
 
 export function merge(intervals: number[][]): number[][] {
-  if (intervals.length <= 1) return intervals;
-
-  let left = 0;
-  let right = 1;
-
-  let result = [...intervals];
-  result.sort((a, b) => a[0] - b[0]);
-
-  while (true) {
-    if (isOverlapped(result[left], result[right])) {
-      const merged = mergeIntervals(result[left], result[right]);
-      result.splice(right, 1);
-      result.splice(left, 1, merged);
-
-      if (left >= result.length - 1) break;
-    } else {
-      right++;
-
-      if (right >= result.length) {
-        left++;
-        if (left >= result.length - 1) break;
-        right = left + 1;
-      }
+  intervals.sort((a, b) => a[0] - b[0]);
+  let n = intervals.length;
+  let results: number[][] = [];
+  let i = 0;
+  while (i < n) {
+    let tmp = new Array(2);
+    tmp[0] = intervals[i][0];
+    let max = intervals[i][1];
+    i++;
+    while (i < n && max >= intervals[i][0]) {
+      if (intervals[i][0] < tmp[0]) tmp[0] = intervals[i][0];
+      max = Math.max(max, intervals[i][1]);
+      i++;
     }
+    tmp[1] = max;
+    results.push(tmp);
   }
-
-  return result;
-}
-
-function mergeIntervals(a: number[], b: number[]) {
-  return [Math.min(a[0], b[0]), Math.max(a[1], b[1])];
-}
-
-function isOverlapped(a: number[], b: number[]) {
-  return (b[0] <= a[0] && a[0] <= b[1]) || (a[0] <= b[0] && b[0] <= a[1]);
+  return results;
 }
