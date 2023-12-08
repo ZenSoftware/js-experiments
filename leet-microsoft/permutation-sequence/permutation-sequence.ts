156478a772801ae9c9b54d65dc132b096715638e
Zen Software <ZenSoftware@users.noreply.github.com>
2023-12-08 01:13:46 -0700

    brute force solution

--- leet-microsoft/permutation-sequence/permutation-sequence.ts
+++ leet-microsoft/permutation-sequence/permutation-sequence.ts
@@ -7,5 +7,37 @@
  */
 
 export function getPermutation(n: number, k: number): string {
-  return '';
+  function perms(elements: number[]) {
+    if (elements.length === 0) return [[]];
+
+    let firstEl = elements[0];
+    let rest = elements.slice(1);
+    let permsWithoutFirst = perms(rest);
+    let result: number[][] = [];
+
+    for (let perm of permsWithoutFirst) {
+      for (let i = 0; i <= perm.length; i++) {
+        const withFirst = [...perm.slice(0, i), firstEl, ...perm.slice(i)];
+        result.push(withFirst);
+      }
+    }
+
+    return result;
+  }
+
+  const nums: number[] = [];
+  for (let i = 1; i <= n; i++) {
+    nums.push(i);
+  }
+
+  const allPerms = perms(nums);
+  allPerms.sort((a, b) => {
+    let result = 0;
+    for (let i = 0; i < n; i++) {
+      result = result || a[i] - b[i];
+    }
+    return result;
+  });
+
+  return allPerms[k - 1].reduce((accum, curr) => (accum += curr.toString()), '');
 }
