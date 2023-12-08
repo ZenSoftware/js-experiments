699097d0df4b1c6bfca6a9745119d647e050ce0a
Zen Software <ZenSoftware@users.noreply.github.com>
2023-12-07 22:35:26 -0700

    skeleton

---
+++ leet-microsoft/spiral-matrix-ii/spiral-matrix-ii.ts
@@ -0,0 +1,10 @@
+/**
+ * https://leetcode.com/problems/spiral-matrix-ii/?envType=featured-list&envId=top-microsoft-questions%3FenvType%3Dfeatured-list&envId=top-microsoft-questions
+ * Given a positive integer n, generate an n x n matrix filled with elements from 1 to n2 in spiral order.
+ * 1 <= n <= 20
+ */
+export function generateMatrix(n: number): number[][] {
+  const result: number[][] = [];
+
+  return result;
+}
