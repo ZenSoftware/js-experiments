47b7177497ce36317567283bcca9f830201e9dc1
Zen Software <ZenSoftware@users.noreply.github.com>
2023-12-07 23:02:48 -0700

    solution

--- leet-microsoft/spiral-matrix-ii/spiral-matrix-ii.ts
+++ leet-microsoft/spiral-matrix-ii/spiral-matrix-ii.ts
@@ -4,7 +4,50 @@
  * 1 <= n <= 20
  */
 export function generateMatrix(n: number): number[][] {
-  const result: number[][] = [];
+  const result: number[][] = new Array(n);
+  for (let i = 0; i < result.length; i++) {
+    result[i] = new Array(n);
+    result[i] = new Array(n);
+  }
+
+  const total = n * n;
+  let top = 0;
+  let bottom = n - 1;
+  let left = 0;
+  let right = n - 1;
+  let count = 1;
+
+  while (count <= total) {
+    for (let col = left; col <= right; col++) {
+      result[top][col] = count;
+      count++;
+      if (count > total) break;
+    }
+    top++;
+
+    for (let row = top; row <= bottom; row++) {
+      result[row][right] = count;
+      count++;
+      if (count > total) break;
+    }
+    right--;
+
+    for (let col = right; col >= left; col--) {
+      result[bottom][col] = count;
+      count++;
+      if (count > total) break;
+    }
+    bottom--;
+
+    for (let row = bottom; row >= top; row--) {
+      result[row][left] = count;
+      count++;
+      if (count > total) break;
+    }
+    left++;
+  }
 
   return result;
 }
+
+// console.log(generateMatrix(3));
