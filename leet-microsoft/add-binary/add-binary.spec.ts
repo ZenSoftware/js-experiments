bc9c5238f34517102aeba722ae2cb4f2d52ee51e
Zen Software <ZenSoftware@users.noreply.github.com>
2023-12-08 07:05:47 -0700

    solution

--- leet-microsoft/add-binary/add-binary.spec.ts
+++ leet-microsoft/add-binary/add-binary.spec.ts
@@ -4,5 +4,6 @@ describe('Add Binary', () => {
   it('evaluates correctly', () => {
     expect(addBinary('11', '1')).toEqual('100');
     expect(addBinary('1010', '1011')).toEqual('10101');
+    expect(addBinary('0', '0')).toEqual('0');
   });
 });
--- leet-microsoft/add-binary/add-binary.ts
+++ leet-microsoft/add-binary/add-binary.ts
@@ -6,5 +6,34 @@
  * Each string does not contain leading zeros except for the zero itself.
  */
 export function addBinary(a: string, b: string): string {
-  return '';
+  const binaryA = a.split('').map(v => parseInt(v));
+  const binaryB = b.split('').map(v => parseInt(v));
+  const longestLength = Math.max(binaryA.length, binaryB.length);
+  let carry = 0;
+  let result = '';
+  for (let i = 0; i < longestLength; i++) {
+    let valA = 0;
+    if (i < binaryA.length) valA = binaryA[binaryA.length - 1 - i];
+
+    let valB = 0;
+    if (i < binaryB.length) valB = binaryB[binaryB.length - 1 - i];
+
+    let sum = valA + valB + carry;
+
+    if (sum === 0 || sum === 2) {
+      result = '0' + result;
+    } else {
+      result = '1' + result;
+    }
+
+    if (sum < 2) {
+      carry = 0;
+    } else {
+      carry = 1;
+    }
+  }
+
+  if (carry === 1) result = '1' + result;
+
+  return result;
 }
