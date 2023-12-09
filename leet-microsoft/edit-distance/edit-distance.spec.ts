d7d5893fe191bf4d8f691aa12efcf10bf992558e
Zen Software <ZenSoftware@users.noreply.github.com>
2023-12-08 23:55:47 -0700

    thanks

--- leet-microsoft/edit-distance/edit-distance.spec.ts
+++ leet-microsoft/edit-distance/edit-distance.spec.ts
@@ -2,7 +2,7 @@ import { minDistance } from './edit-distance';
 
 describe('Edit Distance', () => {
   it('evaluates correctly', () => {
-    expect(minDistance('horse', 'ros')).toEqual(5);
+    expect(minDistance('horse', 'ros')).toEqual(3);
     expect(minDistance('intention', 'execution')).toEqual(5);
   });
 });
--- leet-microsoft/edit-distance/edit-distance.ts
+++ leet-microsoft/edit-distance/edit-distance.ts
@@ -12,5 +12,27 @@
 // Solution: https://www.youtube.com/watch?v=XYi2-LPrwm4&t=21s&ab_channel=NeetCode
 
 export function minDistance(word1: string, word2: string): number {
-  return 0;
+  const grid = [...Array(word1.length + 1)].map(() => Array(word2.length + 1));
+
+  for (let i = 0; i <= word1.length; i++) {
+    grid[i][word2.length] = word1.length - i;
+  }
+
+  for (let j = 0; j < word2.length; j++) {
+    grid[word1.length][j] = word2.length - j;
+  }
+
+  for (let i = word1.length - 1; i >= 0; i--) {
+    for (let j = word2.length - 1; j >= 0; j--) {
+      let c1 = word1.charAt(i);
+      let c2 = word2.charAt(j);
+      if (c1 === c2) {
+        grid[i][j] = grid[i + 1][j + 1];
+      } else {
+        grid[i][j] = 1 + Math.min(grid[i][j + 1], grid[i + 1][j], grid[i + 1][j + 1]);
+      }
+    }
+  }
+
+  return grid[0][0];
 }
