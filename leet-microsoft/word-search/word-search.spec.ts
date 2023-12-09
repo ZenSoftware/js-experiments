fe6d901f7070ce5524d24c4cf0d1f0c71de4bbe9
Zen Software <ZenSoftware@users.noreply.github.com>
2023-12-09 04:19:07 -0700

    skeleton

---
+++ leet-microsoft/word-search/word-search.spec.ts
@@ -0,0 +1,38 @@
+import { exist } from './word-search';
+
+describe('Word Search', () => {
+  it('evaluates correctly', () => {
+    expect(
+      exist(
+        [
+          ['A', 'B', 'C', 'E'],
+          ['S', 'F', 'C', 'S'],
+          ['A', 'D', 'E', 'E'],
+        ],
+        'ABCCED'
+      )
+    ).toEqual(true);
+
+    expect(
+      exist(
+        [
+          ['A', 'B', 'C', 'E'],
+          ['S', 'F', 'C', 'S'],
+          ['A', 'D', 'E', 'E'],
+        ],
+        'SEE'
+      )
+    ).toEqual(true);
+
+    expect(
+      exist(
+        [
+          ['A', 'B', 'C', 'E'],
+          ['S', 'F', 'C', 'S'],
+          ['A', 'D', 'E', 'E'],
+        ],
+        'ABCB'
+      )
+    ).toEqual(false);
+  });
+});
---
+++ leet-microsoft/word-search/word-search.ts
@@ -0,0 +1,8 @@
+/**
+ * https://leetcode.com/problems/word-search/
+ * Given an m x n grid of characters board and a string word, return true if word exists in the grid.
+ * The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. The same letter cell may not be used more than once.
+ */
+export function exist(board: string[][], word: string): boolean {
+  return false;
+}
