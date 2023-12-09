206b7b9e81e24f06fd49f0d1c32836592a80a7ad
Zen Software <ZenSoftware@users.noreply.github.com>
2023-12-08 23:59:14 -0700

    skeleton

---
+++ leet-microsoft/set-matrix-zeroes/set-matrix-zeroes.spec.ts
@@ -0,0 +1,29 @@
+import { setZeroes } from './set-matrix-zeroes';
+
+describe('Set Matrix Zeroes', () => {
+  it('evaluates correctly', () => {
+    expect(
+      setZeroes([
+        [1, 1, 1],
+        [1, 0, 1],
+        [1, 1, 1],
+      ])
+    ).toEqual([
+      [1, 0, 1],
+      [0, 0, 0],
+      [1, 0, 1],
+    ]);
+
+    expect(
+      setZeroes([
+        [0, 1, 2, 0],
+        [3, 4, 5, 2],
+        [1, 3, 1, 5],
+      ])
+    ).toEqual([
+      [0, 0, 0, 0],
+      [0, 4, 5, 0],
+      [0, 3, 1, 0],
+    ]);
+  });
+});
---
+++ leet-microsoft/set-matrix-zeroes/set-matrix-zeroes.ts
@@ -0,0 +1,10 @@
+/**
+ * https://leetcode.com/problems/set-matrix-zeroes/
+ * Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
+ * You must do it in place.
+ * m == matrix.length
+ * n == matrix[0].length
+ * 1 <= m, n <= 200
+ * -231 <= matrix[i][j] <= 231 - 1
+ */
+export function setZeroes(matrix: number[][]): void {}
