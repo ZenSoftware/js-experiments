0e81e31154ab4cfe77904556c16c0954fe6d8cdf
Zen Software <ZenSoftware@users.noreply.github.com>
2023-12-09 13:49:59 -0700

    solution

--- leet-microsoft/contains-duplicate/contains-duplicate.ts
+++ leet-microsoft/contains-duplicate/contains-duplicate.ts
@@ -5,5 +5,10 @@
  * -109 <= nums[i] <= 109
  */
 export function containsDuplicate(nums: number[]): boolean {
+  const hash: Record<string, true> = {};
+  for (let num of nums) {
+    if (hash[num]) return true;
+    hash[num] = true;
+  }
   return false;
 }
