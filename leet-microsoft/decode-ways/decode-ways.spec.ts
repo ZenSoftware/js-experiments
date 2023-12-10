682994530a53e0955432357ad8aeb7768e9ad697
Zen Software <ZenSoftware@users.noreply.github.com>
2023-12-10 02:37:59 -0700

    tests

--- leet-microsoft/decode-ways/decode-ways.spec.ts
+++ leet-microsoft/decode-ways/decode-ways.spec.ts
@@ -2,9 +2,9 @@ import { numDecodings } from './decode-ways';
 
 describe('Decode Ways', () => {
   it('evaluates correctly', () => {
-    // expect(numDecodings('12')).toEqual(2);
-    // expect(numDecodings('226')).toEqual(3);
-    // expect(numDecodings('06')).toEqual(0);
-    expect(numDecodings('111111111111111111111111111111111111111111111')).toEqual(0);
+    expect(numDecodings('12')).toEqual(2);
+    expect(numDecodings('226')).toEqual(3);
+    expect(numDecodings('06')).toEqual(0);
+    // expect(numDecodings('111111111111111111111111111111111111111111111')).toEqual(0);
   });
 });
