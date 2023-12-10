75329cd8cb5d12655747a283c06b2148a8e606a9
Zen Software <ZenSoftware@users.noreply.github.com>
2023-12-09 23:31:07 -0700

    progress

--- leet-microsoft/gray-code/gray-code.ts
+++ leet-microsoft/gray-code/gray-code.ts
@@ -24,7 +24,9 @@ export function grayCode(n: number): number[] {
     return binaryString;
   });
 
-  standardStringSequence.map(x => {});
+  standardStringSequence.map(x => {
+    return x;
+  });
 
   return standardSequence;
 }
