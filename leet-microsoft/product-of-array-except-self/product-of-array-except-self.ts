debfe45fe32b2ed6cee3de07a416a284abc946ba
Zen Software <ZenSoftware@users.noreply.github.com>
2023-12-09 17:22:42 -0700

    efficiency

--- leet-microsoft/product-of-array-except-self/product-of-array-except-self.ts
+++ leet-microsoft/product-of-array-except-self/product-of-array-except-self.ts
@@ -16,20 +16,20 @@ export function productExceptSelf(nums: number[]): number[] {
   const postfix = Array(length);
   const result = Array(length);
 
-  for (let i = 0; i < length; i++) {
-    if (i === 0) prefix[i] = nums[i];
-    else prefix[i] = nums[i] * prefix[i - 1];
+  prefix[0] = nums[0];
+  for (let i = 1; i < length; i++) {
+    prefix[i] = nums[i] * prefix[i - 1];
   }
 
-  for (let i = length - 1; i >= 0; i--) {
-    if (i === length - 1) postfix[i] = nums[i];
-    else postfix[i] = nums[i] * postfix[i + 1];
+  postfix[length - 1] = nums[length - 1];
+  for (let i = length - 2; i >= 0; i--) {
+    postfix[i] = nums[i] * postfix[i + 1];
   }
 
-  for (let i = 0; i < length; i++) {
-    if (i === 0) result[i] = postfix[1];
-    else if (i === length - 1) result[i] = prefix[i - 1];
-    else result[i] = prefix[i - 1] * postfix[i + 1];
+  result[0] = postfix[1];
+  result[length - 1] = prefix[length - 2];
+  for (let i = 1; i < length - 1; i++) {
+    result[i] = prefix[i - 1] * postfix[i + 1];
   }
 
   return result;
