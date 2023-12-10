10e3dc5de900bebc1bdbb287fd63823fa60d034f
Zen Software <ZenSoftware@users.noreply.github.com>
2023-12-09 17:15:15 -0700

    solution

--- leet-microsoft/product-of-array-except-self/product-of-array-except-self.ts
+++ leet-microsoft/product-of-array-except-self/product-of-array-except-self.ts
@@ -7,12 +7,30 @@
  * -30 <= nums[i] <= 30
  * The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
  */
+
+// Solution: https://www.youtube.com/watch?v=bNvIQI2wAjk&ab_channel=NeetCode
+
 export function productExceptSelf(nums: number[]): number[] {
-  const result = Array(nums.length).fill(1);
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = 0; j < nums.length; j++) {
-      if (i !== j) result[i] *= nums[j];
-    }
+  const length = nums.length;
+  const prefix = Array(length);
+  const postfix = Array(length);
+  const result = Array(length);
+
+  for (let i = 0; i < length; i++) {
+    if (i === 0) prefix[i] = nums[i];
+    else prefix[i] = nums[i] * prefix[i - 1];
   }
+
+  for (let i = length - 1; i >= 0; i--) {
+    if (i === length - 1) postfix[i] = nums[i];
+    else postfix[i] = nums[i] * postfix[i + 1];
+  }
+
+  for (let i = 0; i < length; i++) {
+    if (i === 0) result[i] = postfix[i + 1];
+    else if (i === length - 1) result[i] = prefix[i - 1];
+    else result[i] = prefix[i - 1] * postfix[i + 1];
+  }
+
   return result;
 }
