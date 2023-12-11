590b52f6097914eb4711c7a699fc3e836d406c45
Zen Software <ZenSoftware@users.noreply.github.com>
2023-12-10 21:20:38 -0700

    comment

--- experiments/dijkstra.ts
+++ experiments/dijkstra.ts
@@ -73,33 +73,33 @@ export class Vertex {
   constructor(public id: string | number, public adjacent: Connection[] = []) {}
 }
 
-const a = new Vertex('a');
-const b = new Vertex('b');
-const c = new Vertex('c');
-const d = new Vertex('d');
-const e = new Vertex('e');
+// const a = new Vertex('a');
+// const b = new Vertex('b');
+// const c = new Vertex('c');
+// const d = new Vertex('d');
+// const e = new Vertex('e');
 
-a.adjacent = [
-  { to: c, distance: 2 },
-  { to: b, distance: 4 },
-];
-b.adjacent = [
-  { to: a, distance: 4 },
-  { to: c, distance: 1 },
-  { to: d, distance: 3 },
-  { to: e, distance: 2 },
-];
-c.adjacent = [
-  { to: a, distance: 2 },
-  { to: b, distance: 1 },
-];
-d.adjacent = [
-  { to: b, distance: 3 },
-  { to: e, distance: 5 },
-];
-e.adjacent = [
-  { to: b, distance: 2 },
-  { to: d, distance: 5 },
-];
+// a.adjacent = [
+//   { to: c, distance: 2 },
+//   { to: b, distance: 4 },
+// ];
+// b.adjacent = [
+//   { to: a, distance: 4 },
+//   { to: c, distance: 1 },
+//   { to: d, distance: 3 },
+//   { to: e, distance: 2 },
+// ];
+// c.adjacent = [
+//   { to: a, distance: 2 },
+//   { to: b, distance: 1 },
+// ];
+// d.adjacent = [
+//   { to: b, distance: 3 },
+//   { to: e, distance: 5 },
+// ];
+// e.adjacent = [
+//   { to: b, distance: 2 },
+//   { to: d, distance: 5 },
+// ];
 
-console.log(dijkstra(a, d, [a, b, c, d, e]));
+// console.log(dijkstra(a, d, [a, b, c, d, e]));
