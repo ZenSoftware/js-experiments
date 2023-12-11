8d7154b85db40f709bf335c175e115c0ef90b683
Zen Software <ZenSoftware@users.noreply.github.com>
2023-12-10 19:56:39 -0700

    heapified

--- experiments/dijkstra.ts
+++ experiments/dijkstra.ts
@@ -2,6 +2,8 @@
  * [Sample Graph](https://www.freecodecamp.org/news/dijkstras-algorithm-explained-with-a-pseudocode-example/)
  */
 
+import { Heap } from 'heap-js';
+
 export function dijkstra(from: Vertex, to: Vertex, graph: Vertex[]) {
   // initialize list of shortest distances
   const record = new Map<Vertex, number>();
@@ -13,20 +15,20 @@ export function dijkstra(from: Vertex, to: Vertex, graph: Vertex[]) {
     record.set(adj.to, adj.distance);
   }
 
-  // create unvisited list
+  // create unvisited lists
   const unvisited = new Map<Vertex, true>(graph.map(v => [v, true]));
 
-  while (unvisited.size > 0) {
+  const unvisitedHeap = new Heap<{ vertex: Vertex; distance: number }>(
+    (a, b) => a.distance - b.distance
+  );
+  for (let v of graph) {
+    unvisitedHeap.push({ vertex: v, distance: Infinity });
+  }
+
+  while (!unvisitedHeap.isEmpty()) {
     // select the vertex with the shortest recorded distance amongst unvisited
-    let shortest!: Vertex;
-    let tmpDistance = Infinity;
-    for (let [v, _] of unvisited) {
-      const distance = record.get(v) as number;
-      if (distance < tmpDistance) {
-        shortest = v;
-        tmpDistance = distance;
-      }
-    }
+    let shortest = unvisitedHeap.pop()!.vertex;
+    unvisited.delete(shortest);
 
     for (let connection of shortest.adjacent) {
       if (unvisited.get(connection.to)) {
@@ -36,11 +38,9 @@ export function dijkstra(from: Vertex, to: Vertex, graph: Vertex[]) {
         if (newDist < recordAdjDist) record.set(connection.to, newDist);
       }
     }
-
-    unvisited.delete(shortest);
   }
 
-  return Array.from(record.entries()).map(([v, dist]) => [v.id, dist]);
+  return record;
 }
 
 export interface Connection {
@@ -52,33 +52,33 @@ export class Vertex {
   constructor(public id: string | number, public adjacent: Connection[] = []) {}
 }
 
-const a = new Vertex('a');
-const b = new Vertex('b');
-const c = new Vertex('c');
-const d = new Vertex('d');
-const e = new Vertex('e');
+// const a = new Vertex('a');
+// const b = new Vertex('b');
+// const c = new Vertex('c');
+// const d = new Vertex('d');
+// const e = new Vertex('e');
 
-a.adjacent = [
-  { to: c, distance: 2 },
-  { to: b, distance: 4 },
-];
-b.adjacent = [
-  { to: a, distance: 4 },
-  { to: c, distance: 1 },
-  { to: d, distance: 3 },
-  { to: e, distance: 2 },
-];
-c.adjacent = [
-  { to: a, distance: 2 },
-  { to: b, distance: 1 },
-];
-d.adjacent = [
-  { to: b, distance: 3 },
-  { to: e, distance: 5 },
-];
-e.adjacent = [
-  { to: b, distance: 2 },
-  { to: d, distance: 5 },
-];
+// a.adjacent = [
+//   { to: c, distance: 2 },
+//   { to: b, distance: 4 },
+// ];
+// b.adjacent = [
+//   { to: a, distance: 4 },
+//   { to: c, distance: 1 },
+//   { to: d, distance: 3 },
+//   { to: e, distance: 2 },
+// ];
+// c.adjacent = [
+//   { to: a, distance: 2 },
+//   { to: b, distance: 1 },
+// ];
+// d.adjacent = [
+//   { to: b, distance: 3 },
+//   { to: e, distance: 5 },
+// ];
+// e.adjacent = [
+//   { to: b, distance: 2 },
+//   { to: d, distance: 5 },
+// ];
 
-console.log(dijkstra(a, d, [a, b, c, d, e]));
+// console.log(dijkstra(a, d, [a, b, c, d, e]));
