e390d917fa46cf7b454f43994eedcb989d543227
Zen Software <ZenSoftware@users.noreply.github.com>
2023-12-10 20:09:36 -0700

    Revert "heapified"
    This reverts commit 8d7154b85db40f709bf335c175e115c0ef90b683.

--- experiments/dijkstra.ts
+++ experiments/dijkstra.ts
@@ -2,8 +2,6 @@
  * [Sample Graph](https://www.freecodecamp.org/news/dijkstras-algorithm-explained-with-a-pseudocode-example/)
  */
 
-import { Heap } from 'heap-js';
-
 export function dijkstra(from: Vertex, to: Vertex, graph: Vertex[]) {
   // initialize list of shortest distances
   const record = new Map<Vertex, number>();
@@ -15,20 +13,20 @@ export function dijkstra(from: Vertex, to: Vertex, graph: Vertex[]) {
     record.set(adj.to, adj.distance);
   }
 
-  // create unvisited lists
+  // create unvisited list
   const unvisited = new Map<Vertex, true>(graph.map(v => [v, true]));
 
-  const unvisitedHeap = new Heap<{ vertex: Vertex; distance: number }>(
-    (a, b) => a.distance - b.distance
-  );
-  for (let v of graph) {
-    unvisitedHeap.push({ vertex: v, distance: Infinity });
-  }
-
-  while (!unvisitedHeap.isEmpty()) {
+  while (unvisited.size > 0) {
     // select the vertex with the shortest recorded distance amongst unvisited
-    let shortest = unvisitedHeap.pop()!.vertex;
-    unvisited.delete(shortest);
+    let shortest!: Vertex;
+    let tmpDistance = Infinity;
+    for (let [v, _] of unvisited) {
+      const distance = record.get(v) as number;
+      if (distance < tmpDistance) {
+        shortest = v;
+        tmpDistance = distance;
+      }
+    }
 
     for (let connection of shortest.adjacent) {
       if (unvisited.get(connection.to)) {
@@ -38,9 +36,11 @@ export function dijkstra(from: Vertex, to: Vertex, graph: Vertex[]) {
         if (newDist < recordAdjDist) record.set(connection.to, newDist);
       }
     }
+
+    unvisited.delete(shortest);
   }
 
-  return record;
+  return Array.from(record.entries()).map(([v, dist]) => [v.id, dist]);
 }
 
 export interface Connection {
@@ -52,33 +52,33 @@ export class Vertex {
   constructor(public id: string | number, public adjacent: Connection[] = []) {}
 }
 
-// const a = new Vertex('a');
-// const b = new Vertex('b');
-// const c = new Vertex('c');
-// const d = new Vertex('d');
-// const e = new Vertex('e');
+const a = new Vertex('a');
+const b = new Vertex('b');
+const c = new Vertex('c');
+const d = new Vertex('d');
+const e = new Vertex('e');
 
-// a.adjacent = [
-//   { to: c, distance: 2 },
-//   { to: b, distance: 4 },
-// ];
-// b.adjacent = [
-//   { to: a, distance: 4 },
-//   { to: c, distance: 1 },
-//   { to: d, distance: 3 },
-//   { to: e, distance: 2 },
-// ];
-// c.adjacent = [
-//   { to: a, distance: 2 },
-//   { to: b, distance: 1 },
-// ];
-// d.adjacent = [
-//   { to: b, distance: 3 },
-//   { to: e, distance: 5 },
-// ];
-// e.adjacent = [
-//   { to: b, distance: 2 },
-//   { to: d, distance: 5 },
-// ];
+a.adjacent = [
+  { to: c, distance: 2 },
+  { to: b, distance: 4 },
+];
+b.adjacent = [
+  { to: a, distance: 4 },
+  { to: c, distance: 1 },
+  { to: d, distance: 3 },
+  { to: e, distance: 2 },
+];
+c.adjacent = [
+  { to: a, distance: 2 },
+  { to: b, distance: 1 },
+];
+d.adjacent = [
+  { to: b, distance: 3 },
+  { to: e, distance: 5 },
+];
+e.adjacent = [
+  { to: b, distance: 2 },
+  { to: d, distance: 5 },
+];
 
-// console.log(dijkstra(a, d, [a, b, c, d, e]));
+console.log(dijkstra(a, d, [a, b, c, d, e]));
