78fa8217c20306f1fd14b209df9c10cfb722e0f5
Zen Software <ZenSoftware@users.noreply.github.com>
2023-12-11 04:09:48 -0700

    solution

--- leet-microsoft/unique-binary-search-trees-ii/unique-binary-search-trees-ii.ts
+++ leet-microsoft/unique-binary-search-trees-ii/unique-binary-search-trees-ii.ts
@@ -4,12 +4,20 @@
  * 1 <= n <= 8
  */
 
-export function generateTrees(n: number): Array<TreeNode | null> {
-  const result: Array<TreeNode | null> = [];
-
-  function dfs() {}
-
-  return [];
+export function generateTrees(end: number, start = 1): Array<TreeNode | null> {
+  if (end < start) return [null];
+  if (end === start) return [new TreeNode(end)];
+  let trees: TreeNode[] = [];
+  for (let i = start; i <= end; i++) {
+    const leftTrees = generateTrees(i - 1, start);
+    const rightTrees = generateTrees(end, i + 1);
+    for (let left of leftTrees) {
+      for (let right of rightTrees) {
+        trees.push(new TreeNode(i, left, right));
+      }
+    }
+  }
+  return trees;
 }
 
 class TreeNode {
