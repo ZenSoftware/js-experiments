eae804f5bf837b2f83d5ac437eb37ef4ba6e4368
Zen Software <ZenSoftware@users.noreply.github.com>
2023-12-11 04:33:02 -0700

    test

--- leet-microsoft/unique-binary-search-trees-ii/unique-binary-search-trees-ii.spec.ts
+++ leet-microsoft/unique-binary-search-trees-ii/unique-binary-search-trees-ii.spec.ts
@@ -2,6 +2,7 @@ import { generateTrees } from './unique-binary-search-trees-ii';
 
 describe('Unique Binary Search Trees II', () => {
   it('evaluates correctly', () => {
-    // expect().toEqual();
+    const result = generateTrees(3);
+    expect(result.length).toEqual(5);
   });
 });
