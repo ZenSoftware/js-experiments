ea45f7b589326f82eaded971020efeb28d8c3698
Zen Software <ZenSoftware@users.noreply.github.com>
2023-12-10 23:41:06 -0700

    skeleton

---
+++ leet-microsoft/unique-binary-search-trees-ii/unique-binary-search-trees-ii.spec.ts
@@ -0,0 +1,7 @@
+import { generateTrees } from './unique-binary-search-trees-ii';
+
+describe('Unique Binary Search Trees II', () => {
+  it('evaluates correctly', () => {
+    // expect().toEqual();
+  });
+});
---
+++ leet-microsoft/unique-binary-search-trees-ii/unique-binary-search-trees-ii.ts
@@ -0,0 +1,20 @@
+/**
+ * https://leetcode.com/problems/unique-binary-search-trees-ii/
+ * Given an integer n, return all the structurally unique BST's (binary search trees), which has exactly n nodes of unique values from 1 to n. Return the answer in any order.
+ * 1 <= n <= 8
+ */
+
+export function generateTrees(n: number): Array<TreeNode | null> {
+  return [];
+}
+
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
