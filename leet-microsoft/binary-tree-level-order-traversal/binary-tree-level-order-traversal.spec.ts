e58fd2fb388e2f3c204387ff8780b6aecccdf7d9
Zen Software <ZenSoftware@users.noreply.github.com>
2023-12-18 21:21:26 -0700

    test

--- leet-microsoft/binary-tree-level-order-traversal/binary-tree-level-order-traversal.spec.ts
+++ leet-microsoft/binary-tree-level-order-traversal/binary-tree-level-order-traversal.spec.ts
@@ -15,4 +15,19 @@ describe('Binary Tree Level Order Traversal', () => {
 
     expect(levelOrder(n3)).toEqual([[3], [9, 20], [15, 7]]);
   });
+
+  it('evaluates correctly 2', () => {
+    const n1 = new TreeNode(1);
+    const n2 = new TreeNode(2);
+    const n3 = new TreeNode(3);
+    const n4 = new TreeNode(4);
+    const n5 = new TreeNode(5);
+
+    n1.left = n2;
+    n1.right = n3;
+    n2.left = n4;
+    n3.right = n5;
+
+    expect(levelOrder(n1)).toEqual([[1], [2, 3], [4, 5]]);
+  });
 });
