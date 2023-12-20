e06d287a6a13802ba2fc83359e52b2c7ee3c3447
Zen Software <ZenSoftware@users.noreply.github.com>
2023-12-19 21:59:46 -0700

    tests

--- leet-microsoft/construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.spec.ts
+++ leet-microsoft/construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.spec.ts
@@ -1,8 +1,15 @@
-import { buildTree } from './construct-binary-tree-from-preorder-and-inorder-traversal';
+import {
+  buildTree,
+  dfsPreorder,
+  dfsInorder,
+} from './construct-binary-tree-from-preorder-and-inorder-traversal';
 
 describe('Construct Binary Tree from Preorder and Inorder Traversal', () => {
   it('evaluates correctly', () => {
-    const result = buildTree([3, 9, 20, 15, 7], [9, 3, 15, 20, 7]);
-    // expect().toEqual();
+    const preorder = [3, 9, 20, 15, 7];
+    const inorder = [9, 3, 15, 20, 7];
+    const result = buildTree(preorder, inorder);
+    expect(dfsPreorder(result)).toEqual(preorder);
+    expect(dfsInorder(result)).toEqual(inorder);
   });
 });
--- leet-microsoft/construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.ts
+++ leet-microsoft/construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.ts
@@ -11,7 +11,18 @@
  */
 
 export function buildTree(preorder: number[], inorder: number[]): TreeNode | null {
-  return null;
+  if (preorder.length === 0) return null;
+
+  const root = new TreeNode(preorder[0]);
+  const nextPreorder = preorder.slice(1);
+
+  const i = inorder.indexOf(preorder[0]);
+  const nextInorder = [...inorder.slice(0, i), ...inorder.slice(i + 1)];
+
+  root.left = buildTree(nextPreorder, inorder.slice(0, i));
+  root.right = buildTree([], inorder.slice(i + 1));
+
+  return root;
 }
 
 class TreeNode {
@@ -24,3 +35,44 @@ class TreeNode {
     this.right = right === undefined ? null : right;
   }
 }
+
+export function dfsPreorder(root: TreeNode | null) {
+  if (!root) return [];
+  const result: number[] = [];
+
+  function search(node: TreeNode) {
+    result.push(node.val);
+    if (node.left) search(node.left);
+    if (node.right) search(node.right);
+  }
+
+  search(root);
+  return result;
+}
+
+export function dfsInorder(root: TreeNode | null) {
+  if (!root) return [];
+  const result: number[] = [];
+
+  function search(node: TreeNode) {
+    if (node.left) search(node.left);
+    result.push(node.val);
+    if (node.right) search(node.right);
+  }
+
+  search(root);
+  return result;
+}
+
+// const n3 = new TreeNode(3);
+// const n9 = new TreeNode(9);
+// const n20 = new TreeNode(20);
+// const n15 = new TreeNode(15);
+// const n7 = new TreeNode(7);
+
+// n3.left = n9;
+// n3.right = n20;
+// n20.left = n15;
+// n20.right = n7;
+
+// console.log(dfsPreorder(n3));
