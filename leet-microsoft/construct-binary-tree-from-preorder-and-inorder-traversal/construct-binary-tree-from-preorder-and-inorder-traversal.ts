d37a1c01edfa24ae798410c02d3cfe5d99405cff
Zen Software <ZenSoftware@users.noreply.github.com>
2023-12-19 22:00:44 -0700

    work

--- leet-microsoft/construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.ts
+++ leet-microsoft/construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.ts
@@ -63,16 +63,3 @@ export function dfsInorder(root: TreeNode | null) {
   search(root);
   return result;
 }
-
-// const n3 = new TreeNode(3);
-// const n9 = new TreeNode(9);
-// const n20 = new TreeNode(20);
-// const n15 = new TreeNode(15);
-// const n7 = new TreeNode(7);
-
-// n3.left = n9;
-// n3.right = n20;
-// n20.left = n15;
-// n20.right = n7;
-
-// console.log(dfsPreorder(n3));
