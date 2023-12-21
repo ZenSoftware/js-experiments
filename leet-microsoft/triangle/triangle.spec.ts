905614da3840db600d933b987ff7783033155e04
Zen Software <ZenSoftware@users.noreply.github.com>
2023-12-20 23:59:07 -0700

    skeleton

---
+++ leet-microsoft/triangle/triangle.spec.ts
@@ -0,0 +1,8 @@
+import { minimumTotal } from './triangle';
+
+describe('Triangle', () => {
+  it('evaluates correctly', () => {
+    expect(minimumTotal([[2], [3, 4], [6, 5, 7], [4, 1, 8, 3]])).toEqual(11);
+    expect(minimumTotal([[-10]])).toEqual(-10);
+  });
+});
---
+++ leet-microsoft/triangle/triangle.ts
@@ -0,0 +1,12 @@
+/**
+ * https://leetcode.com/problems/triangle/?envType=featured-list&envId=top-microsoft-questions%3FenvType%3Dfeatured-list&envId=top-microsoft-questions
+ * Given a triangle array, return the minimum path sum from top to bottom.
+ * For each step, you may move to an adjacent number of the row below. More formally, if you are on index i on the current row, you may move to either index i or index i + 1 on the next row.
+ * 1 <= triangle.length <= 200
+ * triangle[0].length == 1
+ * triangle[i].length == triangle[i - 1].length + 1
+ * -104 <= triangle[i][j] <= 104
+ */
+export function minimumTotal(triangle: number[][]): number {
+  return 0;
+}
