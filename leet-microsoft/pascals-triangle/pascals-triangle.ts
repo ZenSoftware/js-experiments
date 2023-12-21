e6eff106ab739db34958b719002615f30c7ba442
Zen Software <ZenSoftware@users.noreply.github.com>
2023-12-20 22:44:18 -0700

    skeleton

---
+++ leet-microsoft/pascals-triangle/pascals-triangle.ts
@@ -0,0 +1,12 @@
+/**
+ * https://leetcode.com/problems/pascals-triangle/?envType=featured-list&envId=top-microsoft-questions%3FenvType%3Dfeatured-list&envId=top-microsoft-questions
+ * Given an integer numRows, return the first numRows of Pascal's triangle.
+ * In Pascal's triangle, each number is the sum of the two numbers directly above it.
+ * 1 <= numRows <= 30
+ */
+
+export function generate(numRows: number): number[][] {
+  const result: number[][] = [];
+
+  return result;
+}
