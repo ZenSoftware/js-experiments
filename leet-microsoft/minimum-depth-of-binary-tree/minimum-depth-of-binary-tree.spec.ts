a4f63fc33aeac02074e51777d5d614e2c35113ab
Zen Software <ZenSoftware@users.noreply.github.com>
2023-12-20 19:58:05 -0700

    test

--- leet-microsoft/minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.spec.ts
+++ leet-microsoft/minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.spec.ts
@@ -1,7 +1,33 @@
 import { minDepth, TreeNode } from './minimum-depth-of-binary-tree';
 
 describe('Minimum Depth of Binary Tree', () => {
-  it('evaluates correctly', () => {
-    // expect().toEqual();
+  it('evaluates correctly 1', () => {
+    const n3 = new TreeNode(3);
+    const n9 = new TreeNode(9);
+    const n20 = new TreeNode(20);
+    const n15 = new TreeNode(15);
+    const n7 = new TreeNode(7);
+
+    n3.left = n9;
+    n3.right = n20;
+    n20.left = n15;
+    n20.right = n7;
+
+    expect(minDepth(n3)).toEqual(2);
+  });
+
+  it('evaluates correctly 2', () => {
+    const n2 = new TreeNode(2);
+    const n3 = new TreeNode(3);
+    const n4 = new TreeNode(4);
+    const n5 = new TreeNode(5);
+    const n6 = new TreeNode(6);
+
+    n2.right = n3;
+    n3.right = n4;
+    n4.right = n5;
+    n5.right = n6;
+
+    expect(minDepth(n2)).toEqual(5);
   });
 });
--- leet-microsoft/minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.ts
+++ leet-microsoft/minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.ts
@@ -37,28 +37,3 @@ export class TreeNode {
     this.right = right === undefined ? null : right;
   }
 }
-
-// const n1 = new TreeNode(1);
-// const n2 = new TreeNode(2);
-// const n3 = new TreeNode(3);
-// const n4 = new TreeNode(4);
-// const n5 = new TreeNode(5);
-// const n6 = new TreeNode(6);
-
-// n1.left = n2;
-// n1.right = n3;
-// n3.left = n4;
-// n3.right = n5;
-
-const n2 = new TreeNode(2);
-const n3 = new TreeNode(3);
-const n4 = new TreeNode(4);
-const n5 = new TreeNode(5);
-const n6 = new TreeNode(6);
-
-n2.right = n3;
-n3.right = n4;
-n4.right = n5;
-n5.right = n6;
-
-console.log(minDepth(n2));
