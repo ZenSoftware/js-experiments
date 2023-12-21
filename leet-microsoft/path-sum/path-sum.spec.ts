9c9c88e4c0330169a65961ab74b7a3b282397df6
Zen Software <ZenSoftware@users.noreply.github.com>
2023-12-20 20:04:49 -0700

    skeleton

---
+++ leet-microsoft/path-sum/path-sum.spec.ts
@@ -0,0 +1,27 @@
+import { hasPathSum, TreeNode } from './path-sum';
+
+describe('Path Sum', () => {
+  it('evaluates correctly', () => {
+    const n5 = new TreeNode(5);
+    const n4 = new TreeNode(4);
+    const n11 = new TreeNode(11);
+    const n7 = new TreeNode(7);
+    const n2 = new TreeNode(2);
+    const n8 = new TreeNode(8);
+    const n13 = new TreeNode(13);
+    const n4_2 = new TreeNode(4);
+    const n1 = new TreeNode(1);
+
+    n5.left = n4;
+    n5.right = n8;
+    n4.left = n11;
+    n11.left = n7;
+    n11.right = n2;
+    n8.left = n13;
+    n8.right = n4_2;
+    n4_2.right = n1;
+
+    expect(hasPathSum(n5, 22)).toEqual(true);
+    expect(hasPathSum(n5, 34)).toEqual(false);
+  });
+});
