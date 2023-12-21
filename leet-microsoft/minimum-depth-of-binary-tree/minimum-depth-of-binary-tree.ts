fdcecc36d24d0f47171b18d8a08677643582cd30
Zen Software <ZenSoftware@users.noreply.github.com>
2023-12-20 19:56:04 -0700

    solution

--- leet-microsoft/minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.ts
+++ leet-microsoft/minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.ts
@@ -8,7 +8,23 @@
  */
 
 export function minDepth(root: TreeNode | null): number {
-  return 0;
+  if (!root) return 0;
+
+  let smallest = Infinity;
+
+  function dfs(node: TreeNode, depth: number) {
+    if (!node.left && !node.right) {
+      if (depth < smallest) smallest = depth;
+    }
+
+    if (depth > smallest) return;
+
+    if (node.left) dfs(node.left, depth + 1);
+    if (node.right) dfs(node.right, depth + 1);
+  }
+
+  dfs(root, 1);
+  return smallest;
 }
 
 export class TreeNode {
@@ -21,3 +37,28 @@ export class TreeNode {
     this.right = right === undefined ? null : right;
   }
 }
+
+// const n1 = new TreeNode(1);
+// const n2 = new TreeNode(2);
+// const n3 = new TreeNode(3);
+// const n4 = new TreeNode(4);
+// const n5 = new TreeNode(5);
+// const n6 = new TreeNode(6);
+
+// n1.left = n2;
+// n1.right = n3;
+// n3.left = n4;
+// n3.right = n5;
+
+const n2 = new TreeNode(2);
+const n3 = new TreeNode(3);
+const n4 = new TreeNode(4);
+const n5 = new TreeNode(5);
+const n6 = new TreeNode(6);
+
+n2.right = n3;
+n3.right = n4;
+n4.right = n5;
+n5.right = n6;
+
+console.log(minDepth(n2));
