50daa5558bb240cae61f6da9692634dd535a3223
Zen Software <ZenSoftware@users.noreply.github.com>
2023-12-20 19:18:37 -0700

    solution

--- leet-microsoft/balanced-binary-tree/balanced-binary-tree.spec.ts
+++ leet-microsoft/balanced-binary-tree/balanced-binary-tree.spec.ts
@@ -1,7 +1,7 @@
 import { isBalanced, TreeNode } from './balanced-binary-tree';
 
 describe('Balanced Binary Tree', () => {
-  it('evaluates correctly', () => {
+  it('evaluates correctly 1', () => {
     const n3 = new TreeNode(3);
     const n9 = new TreeNode(9);
     const n20 = new TreeNode(20);
@@ -14,5 +14,24 @@ describe('Balanced Binary Tree', () => {
     n20.right = n7;
 
     expect(isBalanced(n3)).toEqual(true);
+  });
+
+  it('evaluates correctly 2', () => {
+    const n1 = new TreeNode(1);
+    const n2 = new TreeNode(2);
+    const n3 = new TreeNode(3);
+    const n4 = new TreeNode(4);
+    const n5 = new TreeNode(5);
+    const n6 = new TreeNode(6);
+    const n7 = new TreeNode(7);
+
+    n1.left = n2;
+    n2.left = n3;
+    n3.left = n4;
+    n3.right = n5;
+    n2.right = n6;
+    n1.right = n7;
+
+    expect(isBalanced(n1)).toEqual(false);
   });
 });
--- leet-microsoft/balanced-binary-tree/balanced-binary-tree.ts
+++ leet-microsoft/balanced-binary-tree/balanced-binary-tree.ts
@@ -5,7 +5,26 @@
  * -104 <= Node.val <= 104
  */
 export function isBalanced(root: TreeNode | null): boolean {
-  return false;
+  if (!root) return true;
+
+  let imabalnceFound = false;
+
+  function dfs(node: TreeNode | null) {
+    if (!node) return 0;
+
+    if (imabalnceFound) return;
+
+    const leftDepth = dfs(node.left);
+    const rightDepth = dfs(node.right);
+
+    const imabalanced = Math.abs(leftDepth - rightDepth) >= 2;
+    if (imabalanced) imabalnceFound = true;
+
+    return 1 + Math.max(leftDepth, rightDepth);
+  }
+
+  dfs(root);
+  return !imabalnceFound;
 }
 
 export class TreeNode {
@@ -18,3 +37,35 @@ export class TreeNode {
     this.right = right === undefined ? null : right;
   }
 }
+
+// const n3 = new TreeNode(3);
+// const n9 = new TreeNode(9);
+// const n20 = new TreeNode(20);
+// const n15 = new TreeNode(15);
+// const n7 = new TreeNode(7);
+
+// n3.left = n9;
+// n3.right = n20;
+// n20.left = n15;
+// n20.right = n7;
+
+// console.log(isBalanced(n3));
+
+//============================================
+
+// const n1 = new TreeNode(1);
+// const n2 = new TreeNode(2);
+// const n3 = new TreeNode(3);
+// const n4 = new TreeNode(4);
+// const n5 = new TreeNode(5);
+// const n6 = new TreeNode(6);
+// const n7 = new TreeNode(7);
+
+// n1.left = n2;
+// n2.left = n3;
+// n3.left = n4;
+// n3.right = n5;
+// n2.right = n6;
+// n1.right = n7;
+
+// console.log(isBalanced(n1));
