37675a7a4e229773508e7d75728648e2afdb2124
Zen Software <ZenSoftware@users.noreply.github.com>
2023-12-21 12:32:26 -0700

    solution

--- leet-microsoft/longest-consecutive-sequence/longest-consecutive-sequence.ts
+++ leet-microsoft/longest-consecutive-sequence/longest-consecutive-sequence.ts
@@ -8,27 +8,20 @@
 export function longestConsecutive(nums: number[]): number {
   if (nums.length === 0) return 0;
 
-  nums.sort((a, b) => a - b);
+  const set = new Set(nums);
+  const cleaned = Array.from(set.values()).sort((a, b) => a - b);
+
   let longest = 1;
-  let count = 1;
 
-  let left = 0;
-  let right = 1;
-
-  while (left < nums.length) {
-    if (nums[right - 1] + 1 === nums[right]) {
-      count++;
-      right++;
-
-      while (nums[right - 1] === nums[right]) {
-        right++;
+  for (let i = 0; i < cleaned.length - longest; i++) {
+    let count = 1;
+    for (let j = i + 1; j < cleaned.length; j++) {
+      if (cleaned[j - 1] + 1 === cleaned[j]) {
+        count++;
+        if (count > longest) longest = count;
+      } else {
+        break;
       }
-
-      if (count > longest) longest = count;
-    } else {
-      count = 1;
-      left++;
-      right = left + 1;
     }
   }
 
