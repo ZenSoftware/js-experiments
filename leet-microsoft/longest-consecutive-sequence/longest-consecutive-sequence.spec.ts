c7dff4e95d6cd91be373197f2740083f7cf2c55e
Zen Software <ZenSoftware@users.noreply.github.com>
2023-12-21 11:53:09 -0700

    skeleton

---
+++ leet-microsoft/longest-consecutive-sequence/longest-consecutive-sequence.spec.ts
@@ -0,0 +1,8 @@
+import { longestConsecutive } from './longest-consecutive-sequence';
+
+describe('Longest Consecutive Sequence', () => {
+  it('evaluates correctly', () => {
+    expect(longestConsecutive([100, 4, 200, 1, 3, 2])).toEqual(4);
+    expect(longestConsecutive([0, 3, 7, 2, 5, 8, 4, 6, 0, 1])).toEqual(9);
+  });
+});
---
+++ leet-microsoft/longest-consecutive-sequence/longest-consecutive-sequence.ts
@@ -0,0 +1,10 @@
+/**
+ * https://leetcode.com/problems/longest-consecutive-sequence/
+ * Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+ * You must write an algorithm that runs in O(n) time.
+ * 0 <= nums.length <= 10^5
+ * -10^9 <= nums[i] <= 10^9
+ */
+export function longestConsecutive(nums: number[]): number {
+  return 0;
+}
