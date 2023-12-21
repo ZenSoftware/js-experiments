cb2a6f53c6e3684511735847591407117cfc5257
Zen Software <ZenSoftware@users.noreply.github.com>
2023-12-20 21:20:08 -0700

    skeleton

---
+++ leet-microsoft/populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.spec.ts
@@ -0,0 +1,30 @@
+import { connect, Node } from './populating-next-right-pointers-in-each-node';
+
+describe('Populating Next Right Pointers in Each Node', () => {
+  it('evaluates correctly', () => {
+    const n1 = new Node(1);
+    const n2 = new Node(2);
+    const n3 = new Node(3);
+    const n4 = new Node(4);
+    const n5 = new Node(5);
+    const n6 = new Node(6);
+    const n7 = new Node(7);
+
+    n1.left = n2;
+    n1.right = n3;
+    n2.left = n4;
+    n2.right = n5;
+    n3.left = n6;
+    n3.right = n7;
+
+    connect(n1);
+
+    expect(n1.next).toEqual(null);
+    expect(n2.next).toEqual(n3);
+    expect(n3.next).toEqual(null);
+    expect(n4.next).toEqual(n5);
+    expect(n5.next).toEqual(n6);
+    expect(n6.next).toEqual(n7);
+    expect(n7.next).toEqual(null);
+  });
+});
---
+++ leet-microsoft/populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.ts
@@ -0,0 +1,31 @@
+/**
+ * https://leetcode.com/problems/populating-next-right-pointers-in-each-node/?envType=featured-list&envId=top-microsoft-questions%3FenvType%3Dfeatured-list&envId=top-microsoft-questions
+ * You are given a perfect binary tree where all leaves are on the same level, and every parent has two children. The binary tree has the following definition:
+ * struct Node {
+ *   int val;
+ *   Node *left;
+ *   Node *right;
+ *   Node *next;
+ * }
+ * Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+ * Initially, all next pointers are set to NULL.
+ * The number of nodes in the tree is in the range [0, 212 - 1].
+ * -1000 <= Node.val <= 1000
+ */
+
+export function connect(root: Node | null): Node | null {
+  return null;
+}
+
+export class Node {
+  val: number;
+  left: Node | null;
+  right: Node | null;
+  next: Node | null;
+  constructor(val?: number, left?: Node, right?: Node, next?: Node) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+    this.next = next === undefined ? null : next;
+  }
+}
