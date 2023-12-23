036a137fb622f76b11858ba55dc27516456116e8
Zen Software <ZenSoftware@users.noreply.github.com>
2023-12-23 01:13:04 -0700

    skeleton

---
+++ leet-microsoft/word-break/word-break.spec.ts
@@ -0,0 +1,9 @@
+import { wordBreak } from './word-break';
+
+describe('Word Break', () => {
+  it('evaluates correctly', () => {
+    expect(wordBreak('leetcode', ['leet', 'code'])).toEqual(true);
+    expect(wordBreak('applepenapple', ['apple', 'pen'])).toEqual(true);
+    expect(wordBreak('catsandog', ['cats', 'dog', 'sand', 'and', 'cat'])).toEqual(false);
+  });
+});
